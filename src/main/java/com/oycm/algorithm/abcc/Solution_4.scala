d23ef7684daafc97e2fad3c28b6499b8011e1dc5
oycm <1164864987@qq.com>
2025-10-30 21:38:50 +0800

    不定长滑动窗口(求子数组个数)-恰好型:992

---
+++ src/main/java/com/oycm/algorithm/abcc/Solution_4.scala
@@ -0,0 +1,22 @@
+package com.oycm.algorithm.abcc
+
+object Solution_4 {
+
+  /**
+   * 992.K 个不同整数的子数组 2210
+   * https://leetcode.cn/problems/subarrays-with-k-different-integers/
+   * nums 正整数数组
+   * k 整数
+   * 好子数组：子数组中不同整数的个数恰好为 k。
+   * - [1,2,1,2,3] k = 2，[1,2,1]
+   * @param nums
+   * @param k 整数
+   * @return
+   */
+  def subarraysWithKDistinct(nums: Array[Int], k: Int): Int = {
+    var ans = 0
+
+    ans
+  }
+
+}
