f5a354f2cfc8b12579c91ccc37eb495047b2c229
oycm <1164864987@qq.com>
2025-11-01 21:06:43 +0800

    不定长滑动窗口(求子数组个数)-其它：2401：题解：滑动窗口 + 或运算添加元素 + 异或运算删除元素

--- src/main/java/com/oycm/algorithm/abcd/Solution_2.scala
+++ src/main/java/com/oycm/algorithm/abcd/Solution_2.scala
@@ -36,8 +36,35 @@ object Solution_2 {
     ans
   }
 
+  def slidingWindow(nums: Array[Int]): Int = {
+    /*
+    题解：滑动窗口 + 或运算添加元素 + 异或运算删除元素
+    r 不断右移，如果都符合条件，每次更新答案
+    当 r 不符合条件时，意味着左端点有元素不符合要求，则需要从子数组中移除元素(异或运算，两个不相同则为 1)，符合条件时，此时就是暴力枚举 r 端点的 ans
+    */
+    var ans = 0
+    // or 里面都是没有 位交集的，和历史元素进行 异或(^) 运算，则可以删除 1 的 bit 位
+    var or = 0
+    var l = 0
+    for (r <- nums.indices) {
+      while ((nums(r) & or) > 0) {
+        // 移除元素
+        or ^= nums(l)
+        l += 1
+      }
+      // 加元素
+      or |= nums(r)
+      ans = Math.max(ans, r - l + 1)
+    }
+
+    ans
+  }
+
   def main(args: Array[String]): Unit = {
     println(longestNiceSubarray(Array(1, 3, 8, 48, 10)) == 3)
     println(longestNiceSubarray(Array(3, 1, 5, 11, 13)) == 1)
+
+    println(slidingWindow(Array(1, 3, 8, 48, 10)) == 3)
+    println(slidingWindow(Array(3, 1, 5, 11, 13)) == 1)
   }
 }
