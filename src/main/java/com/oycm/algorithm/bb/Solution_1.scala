3057459e8361b24de9b55d609f152e3c489db146
ouyangcm <mingorg@163.com>
2025-11-07 10:50:42 +0800

    双序列双指针-2109

--- src/main/java/com/oycm/algorithm/bb/Solution_1.scala
+++ src/main/java/com/oycm/algorithm/bb/Solution_1.scala
@@ -1,4 +1,4 @@
-package com.oycm.algorithm.bb.a
+package com.oycm.algorithm.bb
 
 object Solution_1 {
 
@@ -25,7 +25,7 @@ object Solution_1 {
      */
     val n = s.length
     val m = spaces.length
-    // 不能之间使用字符串拼接
+    // 不能直接使用字符串拼接
     val ans = new StringBuilder(s.substring(0, spaces(0)))
     for (i <- spaces.indices if i < m - 1) {
       ans.append(" ")
@@ -36,9 +36,30 @@ object Solution_1 {
     ans.toString
   }
 
+  def doublePoint(s: String, spaces: Array[Int]): String = {
+    /*
+    初始化 j = 0, 遍历 s，如果 j < m && i == spaces(j) j++ 先添加空格，再追加字符
+     */
+    val ans = new StringBuilder()
+    var j = 0
+    for (i <- s.indices) {
+      if (j < spaces.length && i == spaces(j)) {
+        ans.append(" ")
+        j += 1
+      }
+      ans.append(s(i))
+    }
+
+    ans.toString
+  }
+
   def main(args: Array[String]): Unit = {
     println(addSpaces("LeetcodeHelpsMeLearn", Array(8, 13, 15)) == "Leetcode Helps Me Learn")
     println(addSpaces("icodeinpython", Array(1, 5, 7, 9)) == "i code in py thon")
     println(addSpaces("spacing", Array(0, 1, 2, 3, 4, 5, 6)) == " s p a c i n g")
+
+    println(doublePoint("LeetcodeHelpsMeLearn", Array(8, 13, 15)) == "Leetcode Helps Me Learn")
+    println(doublePoint("icodeinpython", Array(1, 5, 7, 9)) == "i code in py thon")
+    println(doublePoint("spacing", Array(0, 1, 2, 3, 4, 5, 6)) == " s p a c i n g")
   }
 }
--- src/main/java/com/oycm/algorithm/bb/a/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * 双序列双指针-双指针
- *
- */
-package com.oycm.algorithm.bb.a;
