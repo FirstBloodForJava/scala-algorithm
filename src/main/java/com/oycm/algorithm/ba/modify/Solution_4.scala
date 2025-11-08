b72c139a237fe82a867adc08f1be13822f15507e
oycm <1164864987@qq.com>
2025-11-08 19:13:00 +0800

    原地修改-2273-数组比较是否相等

---
+++ src/main/java/com/oycm/algorithm/ba/modify/Solution_4.scala
@@ -0,0 +1,45 @@
+package com.oycm.algorithm.ba.modify
+
+object Solution_4 {
+
+  /**
+   * 2273. 移除字母异位词后的结果数组
+   * https://leetcode.cn/problems/find-resultant-array-after-removing-anagrams/description/
+   *
+   * 字母异位词：是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母通常恰好只用一次。例如，"dacb" 是 "abdc" 的一个字母异位词。
+   *
+   * @param words 小写英文字母组成的字符串数组
+   * @return
+   */
+  def removeAnagrams(words: Array[String]): List[String] = {
+    /*
+    时间复杂度 O(n L), n 是数组的长度, L 是字符串数组中的最长字符
+    */
+    if (words.length == 1) {
+      return words.toList
+    }
+    var preTemp = Array.fill(26)(0)
+    for (i <- words(0).indices) {
+      preTemp(words(0)(i) - 'a') += 1
+    }
+    val ans = scala.collection.mutable.ArrayBuffer[String]()
+    ans.append(words(0))
+    for (i <- 1 until words.length) {
+      val temp = Array.fill(26)(0)
+      for (j <- words(i).indices) {
+        temp(words(i)(j) - 'a') += 1
+      }
+      if (preTemp.toSeq != temp.toSeq) {
+        preTemp = temp
+        ans.append(words(i))
+      }
+    }
+
+    ans.toList
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(removeAnagrams(Array("abba", "baba", "bbaa", "cd", "cd")))
+    println(removeAnagrams(Array("a", "b", "c", "d", "e")))
+  }
+}
