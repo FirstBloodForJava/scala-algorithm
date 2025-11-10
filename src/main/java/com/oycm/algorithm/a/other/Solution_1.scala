363d949c9f2cd60c1f5a6854a769550ea1d22127
ouyangcm <mingorg@163.com>
2025-11-10 13:06:51 +0800

    定长滑动窗口-其它-2200-题解优化

--- src/main/java/com/oycm/algorithm/a/other/Solution_1.scala
+++ src/main/java/com/oycm/algorithm/a/other/Solution_1.scala
@@ -41,9 +41,48 @@ object Solution_1 {
     ans.toList
   }
 
+  def optimize(nums: Array[Int], key: Int, k: Int): List[Int] = {
+    /*
+    nums(j) == key时，[j-k, j+k] 范围的下标都符合
+    题解优化：
+    在区间 [max(i−k,0),min(i+k,n−1)] 是否存在元素等于 key
+    至多 2k+1 长的滑动窗口
+
+    记录 key 最后一次出现的位置 last，last 在窗口中，就把 i 加入答案。
+
+    计算 [0, k-1] 窗口中的位置，i-k <= last 则记录答案
+    后续 只要 i+k < n && nums(i+k) == key 更新 last, i-k <= last 则符合要求
+    初始化 last = -k - 1，在 [0, k-1] 没有符合要求的 j
+     */
+    val n = nums.length
+    val ans = scala.collection.mutable.ArrayBuffer[Int]()
+    var last = -k - 1
+    for (i <- k - 1 to 0 by -1 if last < 0) {
+      if (nums(i) == key) {
+        last = i
+      }
+    }
+
+    for (j <- nums.indices) {
+      if (j + k < n && nums(j + k) == key) {
+        last = j + k
+      }
+      // [0, k-1] 没有符合要求的 j，则 last = -k - 1，会一直不符合要求
+      // 更新 last 时，j 其实是最左边符合要求，直到 j - k > last 会不记录答案
+      if (j - k <= last) {
+        ans.append(j)
+      }
+    }
+
+    ans.toList
+  }
+
   def main(args: Array[String]): Unit = {
     println(findKDistantIndices(Array(3, 4, 9, 1, 3, 9, 5), 9, 1))
     println(findKDistantIndices(Array(2, 2, 2, 2, 2), 2, 2))
+
+    println(optimize(Array(3, 4, 9, 1, 3, 9, 5), 9, 1))
+    println(optimize(Array(2, 2, 2, 2, 2), 2, 2))
   }
 
 
