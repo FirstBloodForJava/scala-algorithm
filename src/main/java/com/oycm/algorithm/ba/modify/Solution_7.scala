128c4fd24e124758fe903fe92673ea198cb018e8
ouyangcm <mingorg@163.com>
2025-11-12 11:05:36 +0800

    原地修改-905-题解：相向双指针原地修改

--- src/main/java/com/oycm/algorithm/ba/modify/Solution_7.scala
+++ src/main/java/com/oycm/algorithm/ba/modify/Solution_7.scala
@@ -1,6 +1,6 @@
 package com.oycm.algorithm.ba.modify
 
-class Solution_7 {
+object Solution_7 {
 
   /**
    * 905. 按奇偶排序数组
@@ -35,4 +35,34 @@ class Solution_7 {
     ans
   }
 
+  def optimizeModify(nums: Array[Int]): Array[Int] = {
+    /*
+    题解：找到最左边的 奇数 l 和 最右边的偶数 r 交换，交换后 l++, r-- 不断循环，知道 l >= r 没有可交换的数时，数组是符合要求的
+
+    相向双指针
+
+    时间复杂度 O(n)
+    空间复杂度 O(1)
+     */
+    var l = 0
+    var r = nums.length - 1
+    while (l < r) {
+      if (nums(l) % 2 == 0) {
+        // 左边，当前是 偶数，查找下一个
+        l += 1
+      } else if (nums(r) % 2 != 0) {
+        // 右边，当前是 奇数，查找下一个
+        r -= 1
+      } else {
+        // 左边是 奇数，右边是 偶数 交换
+        val temp = nums(l)
+        nums(l) = nums(r)
+        nums(r) = temp
+        l += 1
+        r -= 1
+      }
+    }
+
+    nums
+  }
 }
