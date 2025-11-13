25a64fad18e070b323d69ed3f2ba13ee12e19b35
ouyangcm <mingorg@163.com>
2025-11-13 10:24:06 +0800

    分组循环：2348

--- src/main/java/com/oycm/algorithm/c/Solution_6.scala
+++ src/main/java/com/oycm/algorithm/c/Solution_6.scala
@@ -33,10 +33,46 @@ object Solution_6 {
     ans
   }
 
+  def groupedLoop(nums: Array[Int]): Long = {
+    /*
+    分组循环，先找到 0 开始点 i，j = i + 1，找 一组 都是 0 的子数组 直到不是 1 的下标，ans += n * (n+1) / 2
+
+    全是 0, 100000 * 100001 / 2 > Int.MaxValue
+
+    时间复杂度 O(n)
+    空间复杂度 O(1)
+     */
+    val n = nums.length
+
+    var ans = 0L
+    var l = 0
+    while (l < n) {
+      if (nums(l) == 0) {
+        var r = l + 1
+        while (r < n && nums(r) == 0) {
+          r += 1
+        }
+        ans += (r - l).toLong * (r - l + 1) / 2
+        l = r
+      }
+
+      l += 1
+
+    }
+    ans
+  }
+
+
   def main(args: Array[String]): Unit = {
     println(zeroFilledSubarray(Array(1, 3, 0, 0, 2, 0, 0, 4)) == 6)
     println(zeroFilledSubarray(Array(0, 0, 0, 2, 0, 0)) == 9)
     println(zeroFilledSubarray(Array(2, 10, 2019)) == 0)
+
+    println(groupedLoop(Array(1, 3, 0, 0, 2, 0, 0, 4)) == 6)
+    println(groupedLoop(Array(0, 0, 0, 2, 0, 0)) == 9)
+    println(groupedLoop(Array(2, 10, 2019)) == 0)
+    println(groupedLoop(Array.fill(100000)(0)) == 100000L * 100001 / 2)
+
   }
 
 }
