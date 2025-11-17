d7d90c343bf42f31f5b2753031c0fc195af7a1d9
ouyangcm <mingorg@163.com>
2025-11-17 09:46:25 +0800

    二分查找-闭区间、左闭右开区间、开区间写法

--- src/main/java/com/oycm/algorithm/da/Solution_1.scala
+++ src/main/java/com/oycm/algorithm/da/Solution_1.scala
@@ -17,23 +17,24 @@ object Solution_1 {
     时间复杂度 O(log n)
     */
     val ans = Array.fill(2)(0)
-    val start = lowerBound(nums, target)
+    val start = lowerBound_2(nums, target)
     if (start == nums.length || nums(start) != target) {
       return Array(-1, -1)
     }
-    val end = lowerBound(nums, target + 1) - 1
+    val end = lowerBound_2(nums, target + 1) - 1
     ans(0) = start
     ans(1) = end
     ans
   }
 
+
   /**
    *
-   * @param nums 非递减数组, 长为 n
+   * @param nums 非递减数组(升序), 长为 n
    * @param x    大于等于 x 的 第一个 index
    * @return
    */
-  def lowerBound(nums: Array[Int], x: Int): Int = {
+  def lowerBound_1(nums: Array[Int], x: Int): Int = {
     var r = nums.length - 1
     var l = 0
     // 存在元素
@@ -55,6 +56,65 @@ object Solution_1 {
       如果 nums(l) >= x, 则 l = l, nums(l) >= x
     */
     l
+  }
+
+  /**
+   * 左闭右开区间写法
+   *
+   * @param nums 非递减数组(升序), 长为 n
+   * @param x 大于等于 x 的 第一个 index
+   * @return
+   */
+  def lowerBound_2(nums: Array[Int], x: Int): Int = {
+    var r = nums.length
+    var l = 0
+    // 存在元素
+    while (l < r) {
+      val mid = l + (r - l) / 2
+      if (nums(mid) >= x) {
+        r = mid
+      } else {
+        l = mid + 1
+      }
+    }
+    /*
+    注意循环结束时: l == r
+  如果存在 nums(i) >= x, 根据循环条件
+    则 nums(r) >= x, nums(l - 1) < x, nums(l) = nums(r) >= x
+  如果不存在 nums(i) >= x, 根据循环条件, 则 l 不断右移, 直到 l = n -1 = r - 1
+
+  所以 l|r 都可以是答案, 当 r < n 时, 就是答案
+  */
+    r
+  }
+
+  /**
+   * 开区间写法
+   * @param nums 非递减数组(升序), 长为 n
+   * @param x 大于等于 x 的 第一个 index
+   * @return
+   */
+  def lowerBound_3(nums: Array[Int], x: Int): Int = {
+    var r = nums.length
+    var l = -1
+    // 存在元素
+    while (l + 1 < r) {
+      val mid = l + (r - l) / 2
+      if (nums(mid) >= x) {
+        r = mid
+      } else {
+        l = mid
+      }
+    }
+    /*
+    注意循环结束时: l + 1 == r, l = r - 1
+  如果存在 nums(i) >= x, 根据循环条件
+    则 nums(r) >= x, nums(l) < x, nums(r-1) < x, nums(r) >= x
+  如果不存在 nums(i) >= x, 根据循环条件, 则 l 不断右移, 直到 l = n - 1 = r - 1
+
+  所以 r 是答案, 当 r < n 时, 就是答案
+  */
+    r
   }
 
   def main(args: Array[String]): Unit = {
