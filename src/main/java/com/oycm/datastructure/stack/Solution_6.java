8cb75860c627dfbbbc845f6121d822871a935ef4
ouyangcm <mingorg@163.com>
2025-12-02 17:32:35 +0800

    栈-946

---
+++ src/main/java/com/oycm/datastructure/stack/Solution_6.java
@@ -0,0 +1,21 @@
+package com.oycm.datastructure.stack;
+
+
+public class Solution_6 {
+
+    /**
+     * 946. <a href="https://leetcode.cn/problems/validate-stack-sequences/description/">验证栈序列</a> 1462
+     * <p>
+     * 只有当它们可能是在最初空栈上进行的推入 push 和弹出 pop 操作序列的结果时，返回 true
+     * <p>
+     * 否则，返回 false
+     *
+     * @param pushed 数组中的值都不重复
+     * @param popped 数组中的值都不重复
+     * @return
+     */
+    public static boolean validateStackSequences(int[] pushed, int[] popped) {
+
+        return true;
+    }
+}
