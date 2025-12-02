2d62845d210f4fc7baa42a63d59707d0caf8de95
oycm <1164864987@qq.com>
2025-12-02 19:04:23 +0800

    栈-946：数组模拟栈

--- src/main/java/com/oycm/datastructure/stack/Solution_6.java
+++ src/main/java/com/oycm/datastructure/stack/Solution_6.java
@@ -15,7 +15,45 @@ public class Solution_6 {
      * @return
      */
     public static boolean validateStackSequences(int[] pushed, int[] popped) {
+        /*
+        用一个数组 stack 记录栈当前情况，cnt 表示栈中元素数量，cnt-1 表示栈顶元素
+        先压栈，判断是否需要弹栈 stack[cnt] == popped[i]
+         */
+        int n = pushed.length;
+        int[] stack = new int[n];
+        int cnt = 0;
+        int i = 0;
+        int j = 0;
+        while (i < n || j < n) {
+            if (i < n && pushed[i] == popped[j]) {
+                i++;
+                j++;
+            } else {
+                // 入栈前，要考虑栈不为空的情况
+                if (i < n) {
+                    if (cnt > 0 && stack[cnt - 1] == popped[j]){
+                        cnt--;
+                        j++;
+                    } else {
+                        stack[cnt++] = pushed[i++];
+                    }
+                } else {
+                    if (stack[cnt - 1] == popped[j]) {
+                        j++;
+                        cnt--;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
 
         return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(validateStackSequences(new int[]{2, 1, 0}, new int[]{1, 2, 0}));
+        System.out.println(validateStackSequences(new int[]{1, 2, 3, 4, 5}, new int[]{4, 5, 3, 2, 1}));
+        System.out.println(validateStackSequences(new int[]{1, 2, 3, 4, 5}, new int[]{4, 3, 5, 1, 2}));
     }
 }
