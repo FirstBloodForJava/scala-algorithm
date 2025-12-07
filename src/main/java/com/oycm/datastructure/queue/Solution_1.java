570eded44e2c57003c9304ead583cd5921136dbd
oycm <1164864987@qq.com>
2025-12-07 10:24:57 +0800

    队列基础-933

---
+++ src/main/java/com/oycm/datastructure/queue/Solution_1.java
@@ -0,0 +1,34 @@
+package src.main.java.com.oycm.datastructure.queue;
+
+import java.util.Deque;
+
+public class Solution_1 {
+
+
+}
+
+/**
+ * 933. 最近的请求次数 1338
+ * https://leetcode.cn/problems/number-of-recent-calls/description/
+ */
+class RecentCounter {
+
+    Deque<Integer> queue = new ArrayDeque<>();
+
+    public RecentCounter() {
+
+    }
+
+    /**
+     * 返回 [t-3000, t] 的历史次数
+     * @param t
+     * @return
+     */
+    public int ping(int t) {
+        while (!queue.isEmpty() && queue.peekFirst() < t - 3000){
+            queue.pollFirst();
+        }
+        queue.addLast(t);
+        return queue.size();
+    }
+}
