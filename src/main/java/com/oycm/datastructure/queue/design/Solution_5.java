b0bf0a6e8cb428488accb3363af6242482a941d5
oycm <1164864987@qq.com>
2025-12-08 18:30:34 +0800

    队列-设计-622

--- src/main/java/com/oycm/datastructure/queue/design/Solution_5.java
+++ src/main/java/com/oycm/datastructure/queue/design/Solution_5.java
@@ -22,16 +22,18 @@ public class Solution_5 {
 //        circularQueue.deQueue();
 
         MyCircularQueue circularQueue = new MyCircularQueue(3); // 设置长度为 3
-        circularQueue.enQueue(1);  // 返回 true
-        circularQueue.enQueue(2);  // 返回 true
-        circularQueue.enQueue(3);  // 返回 true
-        circularQueue.enQueue(4);  // 返回 false，队列已满
-        circularQueue.Rear();  // 返回 3
-        circularQueue.isFull();  // 返回 true
-        circularQueue.deQueue();  // 返回 true
-        circularQueue.enQueue(4);  // 返回 true
-        circularQueue.Rear();  // 返回 4
+        circularQueue.enQueue(3);
+        circularQueue.Rear();
+        circularQueue.Rear();
+        circularQueue.deQueue();
 
+        circularQueue.enQueue(3);
+        circularQueue.Rear();
+        circularQueue.deQueue();
+
+        circularQueue.Front();
+        circularQueue.deQueue();
+        circularQueue.deQueue();
     }
 
 }
@@ -96,7 +98,8 @@ class MyCircularQueue {
     }
 
     public boolean isEmpty() {
-        return front == capacity;
+        // 这里写错了
+        return front == rear;
     }
 
     public boolean isFull() {
