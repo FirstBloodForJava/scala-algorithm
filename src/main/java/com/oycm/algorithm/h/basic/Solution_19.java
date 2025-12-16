1b28c72e75b1454bccc73da05f632fc66486e2ed
ouyangcm <mingorg@163.com>
2025-12-16 09:29:01 +0800

    位运算-基础-2154-位运算优化

--- src/main/java/com/oycm/algorithm/h/basic/Solution_19.java
+++ src/main/java/com/oycm/algorithm/h/basic/Solution_19.java
@@ -1,7 +1,6 @@
 package com.oycm.algorithm.h.basic;
 
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 public class Solution_19 {
@@ -16,7 +15,7 @@ public class Solution_19 {
     public int findFinalValue(int[] nums, int original) {
         /*
         查找 nums 中是否存在 original 存在 则 original * 2 继续查找，找不到则结束查找
-        todo 优化思路 nums 元素要是 original 2^k 倍数
+
          */
         Set<Integer> set = new HashSet<>();
         for (int num : nums) {
@@ -26,5 +25,44 @@ public class Solution_19 {
             original *= 2;
         }
         return original;
+    }
+
+    public int optimizeSet(int[] nums, int original) {
+        /*
+        set 只记录 original 的 2 的幂次 值
+         */
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            int k = num / original;
+            if (num % original == 0 && (k & (k - 1)) == 0) {
+                set.add(num);
+            }
+        }
+        while (set.contains(original)) {
+            original *= 2;
+        }
+        return original;
+    }
+
+    public int optimizeSpace(int[] nums, int original) {
+        /*
+        主要看所有 k 进行或运算后，最低位的 0 在哪里，结果就是 original * (1 << idx)
+        现在就是怎么找出最低位的 0 的位置，取反后最低位的1
+            1101 连续 + 1 可进位 与取反的交集就是 低位 0 对应的值
+            0010
+            (cnt + 1) & ~cnt
+        也可以通过 取反和取反的负值找最低位 1
+            cnt = ~cnt
+            cnt & -cnt
+         */
+        int cnt = 0;
+        for (int num : nums) {
+            int k = num / original;
+            if (num % original == 0 && (k & (k - 1)) == 0) {
+                cnt |= k;
+            }
+        }
+
+        return original * ((cnt + 1) & ~cnt);
     }
 }
