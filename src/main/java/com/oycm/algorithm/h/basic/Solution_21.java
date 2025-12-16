642792551910c34c22c46a0b5ad64573831e0921
ouyangcm <mingorg@163.com>
2025-12-16 11:27:37 +0800

    位运算-基础-3690

---
+++ src/main/java/com/oycm/algorithm/h/basic/Solution_21.java
@@ -0,0 +1,29 @@
+package com.oycm.algorithm.h.basic;
+
+
+public class Solution_21 {
+
+    /**
+     * 3690. <a href="https://leetcode.cn/problems/split-and-merge-array-transformation/description/">拆分合并数组</a> 1982
+     * <p>
+     * 拆分合并操作：
+     * <p>
+     * - 选择一个子数组 nums1[L..R]。
+     * <p>
+     * - 移除该子数组，留下前缀 nums1[0..L-1]（如果 L=0 则为空）和后缀 nums1[R+1..n-1]（如果 R=n-1 则为空）
+     * <p>
+     * - 将移除的子数组（原顺序）重新插入到剩余数组的任意位置（最开始或最后面）
+     *
+     * @param nums1 长度为 n 的整数数组
+     * @param nums2 长度为 n 的整数数组
+     * @return 求 nums1 转换成 nums2 的最少拆分合并操作次数
+     */
+    public int minSplitMerge(int[] nums1, int[] nums2) {
+        /*
+        todo BFS
+         */
+
+        return 1;
+    }
+
+}
