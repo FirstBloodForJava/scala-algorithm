1078e7b873d8139399d8609a273f2e88681ce7af
ouyangcm <mingorg@163.com>
2025-12-17 11:22:59 +0800

    异或运算-1442: 异或前缀和+哈希表优化

--- src/main/java/com/oycm/algorithm/h/xor/Solution_8.java
+++ src/main/java/com/oycm/algorithm/h/xor/Solution_8.java
@@ -1,5 +1,8 @@
 package com.oycm.algorithm.h.xor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution_8 {
 
     /**
@@ -35,18 +38,51 @@ public class Solution_8 {
         return ans;
     }
 
-    public int hashOptimize(int[] arr) {
+    public static int hashOptimize(int[] arr) {
         /*
         因为 i < j <= k, 其实这里就是二元组问题
-        [] 表示连续子数组的异或和 [0,1] ^ [0,3] == [2,3]
+        [l, r] 表示连续子数组的异或和 [0,1] ^ [0,3] == [2,3]
         s 记为 arr 的异或前缀和
         a = s[i] ^ s[j]
         b = s[j] ^ s[k+1]
-        a == b => s[i] ^ s[j] == s[j] ^ s[k+1]
+        a == b => s[i] ^ s[j] == s[j] ^ s[k+1], a == b, a ^ i == b ^ i, 可以两边同时 ^ s[j]
         即 s[i] == s[k+1] 时, 该区间的答案就是 k - i
+        枚举右(i+1), 维护左(i) 可以省略 前缀异或和 的创建过程
         // todo 使用 hash 表优化
-         */
 
-        return 0;
+        如果 相同的异或前缀和 xor = s[i1] = s[i2] ...
+        s[k+1] ^ xor == 0, 则三元组有 (k - i1) + (k - i2) + ... = k * m + (i1 + i2 + ...) m 表示 相同异或和的个数， i1 + i2 + ... 表示相同异或和的下标
+
+        */
+        int ans = 0;
+        int n = arr.length;
+        // 相同个数和
+        Map<Integer, Integer> cnt = new HashMap<>();
+        // 相同下标和
+        Map<Integer, Integer> total = new HashMap<>();
+
+        /*for (int i = 0; i <= n; i++) {
+            int xor = s[i];
+            ans += cnt.getOrDefault(xor, 0) * (i - 1) - total.getOrDefault(xor, 0);
+
+            cnt.merge(xor, 1, Integer::sum);
+            total.merge(xor, i, Integer::sum);
+        }*/
+        int cur = 0;
+        for (int i = 0; i < n; i++) {
+            cur ^= arr[i];
+            if (cnt.containsKey(cur)) {
+                ans += cnt.get(cur) * i - total.get(cur);
+            }
+            int pre = cur ^ arr[i];
+            cnt.merge(pre, 1, Integer::sum);
+            total.merge(pre, i, Integer::sum);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        hashOptimize(new int[]{2, 3, 1, 6, 7});
     }
 }
