c607fce073565339734eaa1ddca3fbc6b62fdc86
oycm <1164864987@qq.com>
2025-12-18 19:33:52 +0800

    logTrick: 3171-题解

--- src/main/java/com/oycm/algorithm/h/log_trick/Solution_1.java
+++ src/main/java/com/oycm/algorithm/h/log_trick/Solution_1.java
@@ -60,7 +60,36 @@ public class Solution_1 {
         return ans;
     }
 
+    public static int logTrick(int[] nums, int k) {
+        /*
+        可以使用一个二重循环暴力计算 or 值和, 外层循环, 从 i = 0 开始, 从左到右; 内存循环从 j = i-1 开始, 从右到左, 更新 nums[j] = nums[j] | nums[i]
+        这样就能枚举所有 i 开始的 从右到左的所有子数组
+        i = 1, 子数组有 nums[1]; nums[0] = nums[0] | nums[1];
+        i = 2, 子数组有 nums[2]; nums[1] = nums[1] | nums[2]; nums[0] = nums[0] | nums[2](由于前面 nums[0] = nums[0] | nums[1], 这里就是 0,1,2)
+        i = 3, 子数组有 nums[3]; nums[2] = nums[2] | nums[3]; nums[1] = nums[1] | nums[3](由于前面 nums[1] = nums[1] | nums[2], 这里就是 1,2,3)
+                        nums[0] = nums[0] | nums[3], nums[0] = 0,1,2 这里就是 0,1,2,3
+        按照上面的计算过程, 能枚举出所有的子数组 or 值
+        把修改后的 nums[i] 看作集合 A[]
+        i = 1, A0 包含 A1
+        i = 2, A0 包含 A1, A1 包含 A2
+        i = 3, A0 包含 A1, A1 包含 A2, A2 包含 A3
+        A0 , A1, A2, A3 在计算 nums[j] | nums[i] 的过程中, 如果 nums[j] | nums[i] == nums[j], 说明后续的在上次已经遍历过了
+         */
+        int ans = Integer.MAX_VALUE;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int x = nums[i];
+            ans = Math.min(ans, Math.abs(x - k));
+            for (int j = i - 1; j >= 0 && (x | nums[j]) != nums[j]; j--) {
+                nums[j] |= x;
+                ans = Math.min(ans, Math.abs(nums[j] - k));
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
-        minimumDifference(new int[]{1,3,1,3}, 2);
+        minimumDifference(new int[]{1, 3, 1, 3}, 2);
     }
 }
