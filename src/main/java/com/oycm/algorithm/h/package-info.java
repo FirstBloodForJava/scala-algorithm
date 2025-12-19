94664b0f527674bb3cd02609321cba3a41d54b26
ouyangcm <mingorg@163.com>
2025-12-19 13:31:28 +0800

    gcd_log_trick 包说明

--- src/main/java/com/oycm/algorithm/h/package-info.java
+++ src/main/java/com/oycm/algorithm/h/package-info.java
@@ -5,6 +5,7 @@
  *     <li>xor 异或运算</li>
  *     <li>andor 与或运算</li>
  *     <li>log_trick LogTrick</li>
+ *     <li>gcd_log_trick GCD logTrick 最大公约数</li>
  * </ol>
  */
 package com.oycm.algorithm.h;
