5cbac91af2b828d833198001ac699900b88fd358
ouyangcm <mingorg@163.com>
2025-12-19 15:37:48 +0800

    gcd-2654-本质是找连续子数组最大公约数是 1 的最短长度

---
+++ src/main/java/com/oycm/algorithm/h/gcd_log_trick/Solution_2.java
@@ -0,0 +1,59 @@
+package com.oycm.algorithm.h.gcd_log_trick;
+
+
+public class Solution_2 {
+
+    /**
+     * 2654. <a href="https://leetcode.cn/problems/minimum-number-of-operations-to-make-all-array-elements-equal-to-1/description/">使数组所有元素变成 1 的最少操作次数</a> 1929
+     * <p>
+     * 0 <= i < n - 1， 可将 nums[i] 或 nums[i+1] 两者之一替换成它们的最大公约数, 记为一次操作
+     *
+     * @param nums
+     * @return 求 将 nums 所有元素变成 1 的最少操作次数, 不能变成 1, 则返回 -1
+     */
+    public int minOperations(int[] nums) {
+        /*
+        怎么样能最快?
+        nums[i] 和 nums[i+1] 的最大公约数有哪几种情况?
+            [1, min(nums[i], nums[i+1])] 1, 大于 1 的公约数, 无论是 nums[i] 还是 nums[i+1] 换成 gcd, 只要 gcd > 1, nums[i] 和 nums[i+1] 的 gcd 都会大于 1
+        找最小的连续子数组长度 使得 nums[i,j] 的最大公约数是 1, ans = j-i + n-1
+        找到 1 之后，和任意的最大公约数都是 1
+        还需要 把 元素是 1 的个数排掉
+         */
+        int ans = -1;
+        int n = nums.length;
+        int min = n;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            int gcd = 0;
+            if (nums[i] == 1) {
+                cnt++;
+            }
+            for (int j = i; j >= 0; j--) {
+                gcd = gcd(gcd, nums[j]);
+                if (gcd == 1) {
+                    min = Math.min(min, i - j);
+                    break;
+                }
+            }
+        }
+        if (min != n) {
+            ans = min + n - 1;
+        }
+        if (cnt > 0) {
+            ans = n - cnt;
+        }
+
+        return ans;
+    }
+
+    public int gcd(int a, int b) {
+        // a >= 0, b >= 0;
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
