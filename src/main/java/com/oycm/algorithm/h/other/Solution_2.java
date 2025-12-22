fb4c1687cca5f68704902c5fe8b362e4d37576eb
ouyangcm <mingorg@163.com>
2025-12-22 17:17:03 +0800

    其他-260-O(n) 空间复杂度

---
+++ src/main/java/com/oycm/algorithm/h/other/Solution_2.java
@@ -0,0 +1,43 @@
+package com.oycm.algorithm.h.other;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution_2 {
+
+    /**
+     * 260. <a href="https://leetcode.cn/problems/single-number-iii/description/">只出现一次的数字 III</a>
+     *
+     * @param nums 恰好有两个元素只出现一次, 其他元素均出现 2 次
+     * @return
+     */
+    public static int[] singleNumber(int[] nums) {
+        /*
+        根据 136 题, nums 的异或和 就是只出现 一次元素的异或和
+        先计算 nums 的所有异或和, xor == a ^ b, xor ^ a == b, 哈希表的方式查找另外的一个值 空间复杂度是 O(n) 的
+         */
+
+        if (nums.length == 2) {
+            return nums;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+            map.merge(num, 1, Integer::sum);
+        }
+        for (int num : nums) {
+            if (map.getOrDefault(num ^ xor, 0) == 1) {
+                return new int[]{num, num ^ xor};
+            }
+        }
+
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        singleNumber(new int[]{7, 1, 7, 2, 6, 6, 3, 1});
+    }
+
+}
