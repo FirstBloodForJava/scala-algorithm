aeba425e157d923d32464c15b72cd2dfb2b549dc
ouyangcm <mingorg@163.com>
2025-12-23 17:25:41 +0800

    位运算-其他-2965

---
+++ src/main/java/com/oycm/algorithm/h/other/Solution_3.java
@@ -0,0 +1,22 @@
+package com.oycm.algorithm.h.other;
+
+
+public class Solution_3 {
+
+    /**
+     * 2965. <a href="https://leetcode.cn/problems/find-missing-and-repeated-values/description/">找出缺失和重复的数字</a> 1245
+     *
+     * @param grid nxn 的矩阵, 值 [1, n^2] 除了 a 出现了 两次, b 缺失之外, 每个整数都恰好出现一次
+     * @return 求 重复的数组 a, 缺失的数字 b, ans[0] = a ，ans[1] = b
+     */
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        /*
+        1 ^ 2 ^ 3 ... ^ n 的异或和 记为 xor1
+        grid 的异或和记为 xor2
+        xor1 ^ xor2 = a ^ b 一边没有 a, 一边没有 b
+         */
+
+        return null;
+    }
+
+}
