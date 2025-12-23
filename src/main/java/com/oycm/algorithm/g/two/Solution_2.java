12f4e0dd9cfebb885dbe418439b5c273e3127b2b
ouyangcm <mingorg@163.com>
2025-12-23 14:37:42 +0800

    850-待解答

---
+++ src/main/java/com/oycm/algorithm/g/two/Solution_2.java
@@ -0,0 +1,20 @@
+package com.oycm.algorithm.g.two;
+
+
+public class Solution_2 {
+
+    /**
+     * 850. <a href="https://leetcode.cn/problems/rectangle-area-ii/description/">矩形面积 II</a> 2236
+     *
+     * @param rectangles rectangle[i] = [x1, y1, x2, y2], (x1, y1) 表示矩形 左下角坐标, (x2, y2) 表示矩形右上角坐标
+     * @return 计算 rectangles 覆盖的总面积, 被两个或多个矩形覆盖的区域只计算一次
+     */
+    public int rectangleArea(int[][] rectangles) {
+        /*
+        每个单元格只能贡献 一个 面积
+        todo 线段树
+         */
+        return 0;
+    }
+
+}
