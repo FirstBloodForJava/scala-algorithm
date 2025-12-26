189b945cec30ccec160e8db8efe02df2a17cd8ff
oycm <1164864987@qq.com>
2025-12-26 20:39:25 +0800

    包说明

--- src/main/java/com/oycm/datastructure/heap/package-info.java
+++ src/main/java/com/oycm/datastructure/heap/package-info.java
@@ -5,6 +5,8 @@
  *     <li>advance 进阶</li>
  *     <li>k 第 k 小/大</li>
  *     <li>rearrange 重排元素</li>
+ *     <li>regret 反悔堆</li>
+ *     <li>lazy 懒删除堆</li>
  * </ol>
  */
 package com.oycm.datastructure.heap;
