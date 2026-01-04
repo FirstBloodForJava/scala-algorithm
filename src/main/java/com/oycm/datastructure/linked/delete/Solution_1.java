bf5cbc0b374bd2d880915e799e0211b7641b1483
ouyangcm <mingorg@163.com>
2026-01-04 21:12:48 +0800

    删除节点

---
+++ src/main/java/com/oycm/datastructure/linked/delete/Solution_1.java
@@ -0,0 +1,19 @@
+package com.oycm.datastructure.linked.delete;
+
+
+import com.oycm.ListNode;
+
+public class Solution_1 {
+
+    /**
+     * 203. <a href="https://leetcode.cn/problems/remove-linked-list-elements/description/">移除链表元素</a>
+     *
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode removeElements(ListNode head, int val) {
+
+    }
+
+}
