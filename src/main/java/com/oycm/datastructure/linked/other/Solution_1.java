cb885c8766fb39b6205f6ad30a7de8840a6e8394
ouyangcm <mingorg@163.com>
2026-01-15 17:09:51 +0800

    交错链表

--- src/main/java/com/oycm/datastructure/linked/other/Solution_1.java
+++ src/main/java/com/oycm/datastructure/linked/other/Solution_1.java
@@ -42,4 +42,40 @@ public class Solution_1 {
         return dummy.next;
     }
 
+    public Node noneHash(Node head) {
+        /*
+        题解思路: 交错链表
+        1 -> 2 -> 3 => 1 -> 1' -> 2 -> 2' -> 3 -> 3' 这样 1 的 random 节点如果指向 3, 则 1' 的 random 指向 1.random.next
+         */
+        if (head == null) {
+            return null;
+        }
+        // 新的交错链表
+        for (Node cur = head; cur != null; cur = cur.next.next) {
+            Node temp = cur.next;
+            cur.next = new Node(cur.val);
+            cur.next.next = temp;
+        }
+        // 遍历交错链表
+        for (Node cur = head; cur != null; cur = cur.next.next) {
+            if (cur.random != null) {
+                cur.next.random = cur.random.next;
+            }
+        }
+        // 分离链表 1 -> 1' -> 2 -> 2' -> 3 -> 3'
+        // 1 -> 2 -> 3; 1' -> 2' -> 3'
+        Node copyHead = head.next;
+        Node cur = head;
+        // 至少四个节点才拆分 1 -> 1' -> 2 -> 2'
+        for (; cur.next.next != null; cur = cur.next) {
+            // 新的节点
+            Node copy = cur.next;
+            cur.next = copy.next;
+            copy.next = copy.next.next;
+        }
+        cur.next = null;
+
+        return copyHead;
+    }
+
 }
