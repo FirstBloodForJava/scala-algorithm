78702e5cb634c6379842d6fcd03ce12ced12679e
ouyangcm <mingorg@163.com>
2026-01-15 17:21:05 +0800

    二分答案-最大值

---
+++ src/main/java/com/oycm/algorithm/d/binary_search_ans_max/Solution_3.java
@@ -0,0 +1,5 @@
+package com.oycm.algorithm.d.binary_search_ans_max;
+
+
+public class Solution_3 {
+}
--- src/main/java/com/oycm/algorithm/d/package-info.java
+++ src/main/java/com/oycm/algorithm/d/package-info.java
@@ -3,6 +3,7 @@
  * <ol>
  *     <li>binary_search 二分查找</li>
  *     <li>binary_search_ans_min 二分答案最小值</li>
+ *     <li>binary_search_ans_max 二分答案最大值</li>
  * </ol>
  */
 
