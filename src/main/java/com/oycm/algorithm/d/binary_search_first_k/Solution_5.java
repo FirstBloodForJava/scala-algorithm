b0537686c0b1c23eabb8421c912240d977b21e60
oycm <1164864987@qq.com>
2026-01-19 18:34:36 +0800

    第 k 小: 二分答案 + 容斥原理 + gcd + lcm

--- src/main/java/com/oycm/algorithm/d/binary_search_first_k/Solution_5.java
+++ src/main/java/com/oycm/algorithm/d/binary_search_first_k/Solution_5.java
@@ -16,13 +16,49 @@ public class Solution_5 {
      */
     public int nthUglyNumber(int n, int a, int b, int c) {
         /*
-        |a ∪ b ∪ c| = |a| + |b| + |c| - |a ∩ b| - |b ∩ c| - |a ∩ c| - |a ∩ b ∩ c|
+        注意 要加上 abcLcm 的数量
+        |a ∪ b ∪ c| = |a| + |b| + |c| - |a ∩ b| - |b ∩ c| - |a ∩ c| + |a ∩ b ∩ c|
         怎么求 a, b,c 的最小公倍数
          */
         long left = 0;
         long right = (long) Math.min(Math.min(a, b), c) * n;
 
+        long abLcm = (long) a * b / gcd(a, b);
+        long acLcm = (long) a * c / gcd(a, c);
+        long bcLcm = (long) b * c / gcd(b, c);
+        long abcLcm = abLcm * c / gcd(abLcm, c);
+        while (left + 1 < right) {
+            long mid = (right + left) / 2;
+            if (check(mid, a, b, c, abLcm, acLcm, bcLcm, abcLcm, n)) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+
         return (int) right;
+    }
+
+    public boolean check(long x, int a, int b, int c, long ab, long ac, long bc, long abc, int n) {
+        return x / a + x / b + x / c - x / ab - x / ac - x / bc + x / abc >= n;
+    }
+
+    public int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 
 }
