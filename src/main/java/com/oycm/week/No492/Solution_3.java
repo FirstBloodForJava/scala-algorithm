d4a4eb3d5ad047b0d5e8286c3c6c13214457ab58
oycm <1164864987@qq.com>
2026-03-08 20:33:01 +0800

    492周赛-260308-题目

---
+++ src/main/java/com/oycm/week/No492/Solution_3.java
@@ -0,0 +1,11 @@
+package com.oycm.week.No492;
+
+public class Solution_3 {
+
+    public int minOperations(String s) {
+        int ans = 0;
+
+        return ans;
+    }
+
+}
---
+++ src/main/java/com/oycm/week/No492/Solution_4.java
@@ -0,0 +1,11 @@
+package com.oycm.week.No492;
+
+public class Solution_4 {
+
+    public long minCost(String s, int encCost, int flatCost) {
+        int ans = 0;
+
+        return ans;
+    }
+
+}
