29e5134dfb22d18f3e7583b1df7f753fcacd513b
ouyangcm <mingorg@163.com>
2026-03-17 10:56:15 +0800

    分治过程说明

--- src/main/java/com/oycm/datastructure/binary_tree/linked_and_tree/Solution_1.java
+++ src/main/java/com/oycm/datastructure/binary_tree/linked_and_tree/Solution_1.java
@@ -7,6 +7,7 @@ public class Solution_1 {
     /**
      * 114. <a href="https://leetcode.cn/problems/flatten-binary-tree-to-linked-list/description/">二叉树展开为链表</a>
      * 先序遍历 二叉树 转换成全部指向 right 的链表
+     *
      * @param root
      */
     public void flatten(TreeNode root) {
@@ -24,7 +25,7 @@ public class Solution_1 {
 
     private TreeNode head;
 
-    static class Solution_1_1{
+    static class Solution_1_1 {
 
         public void flatten(TreeNode root) {
             /*
@@ -33,6 +34,12 @@ public class Solution_1 {
             dfs(root);
         }
 
+        /**
+         * <img src = "http://47.101.155.205/image-20260317105459818.png" alt = "dfs 过程说明">
+         *
+         * @param node
+         * @return
+         */
         public TreeNode dfs(TreeNode node) {
             if (node == null) return null;
             TreeNode leftTail = dfs(node.left);
@@ -45,6 +52,24 @@ public class Solution_1 {
             return rightTail != null ? rightTail : leftTail != null ? leftTail : node;
         }
 
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode node1L1 = new TreeNode(2);
+        TreeNode node1R1 = new TreeNode(7, new TreeNode(8), new TreeNode(9));
+        root.left = node1L1;
+        root.right = node1R1;
+        TreeNode node2L1 = new TreeNode(3);
+        node1L1.left = node2L1;
+        TreeNode node3L1 = new TreeNode(4);
+        TreeNode node3R1 = new TreeNode(5);
+        node2L1.left = node3L1;
+        node2L1.right = node3R1;
+        node3R1.left = new TreeNode(6);
+
+        new Solution_1_1().flatten(root);
+        System.out.println(root);
     }
 
 }
