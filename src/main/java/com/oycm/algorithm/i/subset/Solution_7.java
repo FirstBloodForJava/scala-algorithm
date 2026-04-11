732e3ef2f4f6bca053832af396ef79dcd4b238d1
oycm <1164864987@qq.com>
2026-04-11 21:54:46 +0800

    子集型回溯-260411- Gosper's Hack 长为 n 的二进制中快速选 m 个为 1 值

--- src/main/java/com/oycm/algorithm/i/subset/Solution_7.java
+++ src/main/java/com/oycm/algorithm/i/subset/Solution_7.java
@@ -57,3 +57,49 @@ public class Solution_7 {
     }
 
 }
+
+class Solution_7_GosperHack {
+
+    public int maximumRows(int[][] matrix, int numSelect) {
+        /*
+        n = 7, numSelect = 4, 快速枚举用 4 个 1 的下一个值 0001111
+        求 0011110 下一个值
+            1. 将最右侧的 01 转换成 10
+            2. 将该位置后的所有 1 移动到最右边
+        第一点, 可以 0011110 + lowbit(0011110) 得到 0100000
+        0011110
+        0100000
+        观察到 0011110 + lowbit 后的值和原值 异或后，可得到 0111110(左边多了 1 个 1), 将后面的 3 个一移动到最右边
+            0111110 / lowbit =>  011111(去掉 lowbit 后面的 0) 再左移 两位
+        计算下一个值的公式
+            left = x + lowbit(x) = x + (x & -x)
+            right = (left ^ x) / lowbit(x) >> 2
+         */
+        int m = matrix.length, n = matrix[0].length;
+        // mask[i] 值表示第 i 行，二进制从低位到高位，为 1 的下标表示 j 列为 1
+        int[] mask = new int[m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                mask[i] |= matrix[i][j] << j;
+            }
+        }
+
+        int ans = 0;
+        int subset = (1 << numSelect) - 1;
+        while (subset < (1 << n)) {
+            int cnt = 0;
+            for (int row : mask) {
+                if ((row & subset) == row) {
+                    cnt++;
+                }
+            }
+            ans = Math.max(ans, cnt);
+            // 计算下一个 numSelect 长的值
+            int lb = subset & -subset;
+            int x = subset + lb;
+            subset = ((subset ^ x) / lb >> 2) | x;
+        }
+        return ans;
+    }
+
+}
