3436ace50aa57a63c4488d8c71adb2bd82bd7010
oycm <1164864987@qq.com>
2026-04-11 17:20:49 +0800

    子集型回溯-260411-二进制枚举

---
+++ src/main/java/com/oycm/algorithm/i/subset/Solution_5.java
@@ -0,0 +1,48 @@
+package com.oycm.algorithm.i.subset;
+
+public class Solution_5 {
+
+    /**
+     * 2044. <a href="https://leetcode.cn/problems/count-number-of-maximum-bitwise-or-subsets/description/">统计按位或能得到最大值的子集数目</a> 1568
+     *
+     * @param nums 整数数组
+     * @return
+     */
+    public int countMaxOrSubsets(int[] nums) {
+        /*
+        找出 nums 子集 按位或 可能得到的 最大值
+        选中的元素下标位置不一样，则认为两个子集 不同(不看元素, 看下标)
+         */
+        return 0;
+    }
+
+}
+
+class Solution_5_BinaryEnum {
+
+    public int countMaxOrSubsets(int[] nums) {
+        /*
+        可以先 或出最大值, 选/不选
+        选完之后，怎么回溯这个 选中的值?
+        用 二进制枚举
+         */
+        int n = nums.length, cnt = 0;
+        // 最大值
+        int max = 0;
+        for (int x : nums) {
+            max |= x;
+        }
+        for (int i = 0; i < (1 << n); i++) {
+            int xor = 0;
+            for (int j = 0; j < n; j++) {
+                if ((i >> j & 1) == 1) {
+                    // 选
+                    xor |= nums[j];
+                }
+            }
+            if (xor == max) cnt++;
+        }
+
+        return cnt;
+    }
+}
