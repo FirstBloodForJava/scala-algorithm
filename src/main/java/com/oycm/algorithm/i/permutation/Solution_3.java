fc8771b51e8e7661bb9e3a3f7c9763a00039d2a8
ouyangcm <mingorg@163.com>
2026-04-17 15:51:01 +0800

    排列型回溯-260417 调整

--- src/main/java/com/oycm/algorithm/i/permutation/Solution_3.java
+++ src/main/java/com/oycm/algorithm/i/permutation/Solution_3.java
@@ -10,7 +10,7 @@ public class Solution_3 {
      * Bob 被困在了一个地窖里，他需要破解 n 个锁才能逃出地窖，每一个锁都需要一定的 能量 才能打开。
      *
      * @param strength 整数数组，strength[i] 表示打开第 i 个锁需要的能量, strength.length [1, 8], strength[i] [1, 1e6]
-     * @param k [1, 10]
+     * @param k        [1, 10]
      * @return 返回 Bob 打开所有 n 把锁需要的 最少 时间。
      */
     public int findMinimumTime(List<Integer> strength, int k) {
@@ -33,19 +33,18 @@ public class Solution_3 {
         枚举打开所有锁顺序的方案，计算需要的总时间
         时间计算 need / x 上取整, need + x - 1 / x
          */
+        // 剪枝
+        if (total >= ans) {
+            return;
+        }
         if (i == strength.size()) {
             ans = Math.min(ans, total);
             return;
         }
         for (int j = 0; j < strength.size(); j++) {
             if (!enable[j]) {
-                int nextTotal = total + (strength.get(j) + x - 1) / x;
-                // 剪枝
-                if (nextTotal >= ans) {
-                    continue;
-                }
                 enable[j] = true;
-                dfs(i + 1, x + k, k, strength, nextTotal, enable);
+                dfs(i + 1, x + k, k, strength, total + (strength.get(j) - 1) / x + 1, enable);
                 enable[j] = false;
             }
         }
