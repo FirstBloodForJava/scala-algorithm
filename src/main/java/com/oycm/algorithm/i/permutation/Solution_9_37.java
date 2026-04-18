efefe1c20b9b0ca90cd1f84c457b6bee171f4237
oycm <1164864987@qq.com>
2026-04-18 15:53:32 +0800

    排列型回溯-260418 解数独，先天更确定的

--- src/main/java/com/oycm/algorithm/i/permutation/Solution_9_37.java
+++ src/main/java/com/oycm/algorithm/i/permutation/Solution_9_37.java
@@ -1,5 +1,9 @@
 package com.oycm.algorithm.i.permutation;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class Solution_9_37 {
 
     /**
@@ -61,6 +65,85 @@ public class Solution_9_37 {
 
 class Solution_9_37_2 {
 
+    public void solveSudoku(char[][] board) {
+        boolean[][] rowHas = new boolean[9][9];
+        boolean[][] colHas = new boolean[9][9];
+        boolean[][][] subBoxHas = new boolean[3][3][9];
+        // 需要填的位置
+        List<int[]> emptyPos = new ArrayList<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    emptyPos.add(new int[]{i, j}); // 记录空格子的位置
+                } else {
+                    int x = board[i][j] - '1';
+                    rowHas[i][x] = colHas[j][x] = subBoxHas[i / 3][j / 3][x] = true;
+                }
+            }
+        }
+
+        PriorityQueue<int[]> emptyPQ = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        for (int[] pos : emptyPos) {
+            int i = pos[0];
+            int j = pos[1];
+            int candidates = getCandidates(i, j, rowHas, colHas, subBoxHas);
+            emptyPQ.offer(new int[]{candidates, i, j}); // 待定数字个数最少的在堆顶
+        }
+
+        dfs(board, rowHas, colHas, subBoxHas, emptyPQ);
+    }
+
+    // 计算 (i, j) 这个空格子的待定数字个数
+    private int getCandidates(int i, int j, boolean[][] rowHas, boolean[][] colHas, boolean[][][] subBoxHas) {
+        int candidates = 9;
+        for (int x = 0; x < 9; x++) {
+            if (rowHas[i][x] || colHas[j][x] || subBoxHas[i / 3][j / 3][x]) {
+                candidates--;
+            }
+        }
+        return candidates;
+    }
+
+    // 每次递归，选一个空格子，枚举填入的数字
+    private boolean dfs(char[][] board, boolean[][] rowHas, boolean[][] colHas, boolean[][][] subBoxHas, PriorityQueue<int[]> emptyPQ) {
+        if (emptyPQ.isEmpty()) { // 所有格子都已填入数字
+            return true; // 完成数独
+        }
+
+        // 数独玩法：优先考虑待定数字个数最少的空格子
+        int[] top = emptyPQ.poll();
+        int i = top[1];
+        int j = top[2];
+
+        int candidates = 0; // 受之前填入的数字影响，实际待定数字个数可能比入堆时的少，需要重新计算
+        // 枚举没填过的数字 x，填入 board[i][j]
+        for (int x = 0; x < 9; x++) {
+            if (rowHas[i][x] || colHas[j][x] || subBoxHas[i / 3][j / 3][x]) {
+                continue; // x 填过了
+            }
+
+            // 填入 board[i][j]
+            board[i][j] = (char) ('1' + x);
+            // 标记行、列、宫包含数字 x
+            rowHas[i][x] = colHas[j][x] = subBoxHas[i / 3][j / 3][x] = true;
+
+            // 填下一个空格子
+            if (dfs(board, rowHas, colHas, subBoxHas, emptyPQ)) {
+                return true; // 完成数独
+            }
+
+            // 恢复现场（撤销）
+            // 注意 board[i][j] 无需恢复现场，因为我们会直接覆盖掉之前填入的数字
+            rowHas[i][x] = colHas[j][x] = subBoxHas[i / 3][j / 3][x] = false;
+            // 统计待定数字个数
+            candidates++;
+        }
+
+        // 回溯
+        emptyPQ.offer(new int[]{candidates, i, j});
+        return false;
+    }
 }
 
 class Solution_9__36 {
