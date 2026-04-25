35bd8822e9850b06bd944721544f896d70e9fdd5
oycm <1164864987@qq.com>
2026-04-25 21:46:49 +0800

    dfs 剪枝

--- src/main/java/com/oycm/algorithm/i/search/Solution_12.java
+++ src/main/java/com/oycm/algorithm/i/search/Solution_12.java
@@ -106,3 +106,69 @@ class Solution_698 {
         return false;
     }
 }
+
+class Solution_2305 {
+
+    /**
+     * 2305. <a href="https://leetcode.cn/problems/fair-distribution-of-cookies/description/">公平分发饼干</a> 1887
+     *
+     * @param cookies 整数数组 cookies ，其中 cookies[i] 表示在第 i 个零食包中的饼干数量。length [2, 8]
+     * @param k       等待分发零食包的孩子数量
+     * @return
+     */
+    public int distributeCookies(int[] cookies, int k) {
+        /*
+        所有 零食包都需要分发，在同一个零食包中的所有饼干都必须分发给同一个孩子，不能分开。
+        分发的不公平程度 定义为单个孩子在分发过程中能够获得饼干的最大总数。
+         */
+        /*
+        优化：
+        剪枝一：保证每个人分一袋饼干
+        剪枝二：第一袋饼干，无论分给谁都一样，固定非给第一个人
+        剪枝三：如果第 j 个人，j > 1 分得的饼干数量和前一个人一样，跳过
+        剪枝四：如果一个人分得的饼干数量 > ans 了，跳过
+         */
+        Arrays.sort(cookies);
+        for (int l = 0, r = cookies.length - 1; l < r; l++, r--) {
+            int temp = cookies[l];
+            cookies[l] = cookies[r];
+            cookies[r] = temp;
+        }
+        dfs(0, cookies, new int[k], 0);
+        return ans;
+    }
+
+    int ans = Integer.MAX_VALUE;
+
+    public void dfs(int i, int[] cookies, int[] path, int max) {
+        if (i == cookies.length) {
+            // 发完了
+            if (max < ans) {
+                ans = max;
+            }
+            return;
+        }
+        int empty = 0;
+        for (int x : path) {
+            if (x == 0) {
+                empty++;
+            }
+        }
+        // 一
+        if (empty > cookies.length - i) return;
+        for (int j = 0; j < path.length; j++) {
+            // 二
+            if (i == 0 && j > 0) {
+                return;
+            }
+            // 三
+            if (j > 0 && path[j] == path[j -1]) continue;
+            // 四
+            if (path[j] + cookies[i] > ans) continue;
+            path[j] += cookies[i];
+            dfs(i + 1, cookies, path, Math.max(path[j], max));
+            path[j] -= cookies[i];
+        }
+    }
+
+}
