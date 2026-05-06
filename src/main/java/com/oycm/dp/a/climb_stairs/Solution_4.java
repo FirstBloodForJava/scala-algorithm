511a164025667b273799e89dbd60508cf144e268
oycm <1164864987@qq.com>
2026-05-06 21:36:13 +0800

    转换成爬楼梯求方案数

---
+++ src/main/java/com/oycm/dp/a/climb_stairs/Solution_4.java
@@ -0,0 +1,40 @@
+package com.oycm.dp.a.climb_stairs;
+
+import java.util.Arrays;
+
+
+public class Solution_4 {
+
+    /**
+     * 377. <a href="https://leetcode.cn/problems/combination-sum-iv/description/">组合总和 Ⅳ</a> 1600
+     *
+     * @param nums 不同 整数组成的数组
+     * @param target 目标整数
+     * @return 从 nums 中找出并返回总和为 target 的元素组合的个数。
+     */
+    public int combinationSum4(int[] nums, int target) {
+        /*
+        题解思路：本质是 70 的爬楼梯，每次从 nums 中选一个数，作为往上爬的台阶数，计算爬 target 个台阶有多少种方案
+         */
+        int[] memo = new int[target + 1];
+        Arrays.fill(memo, -1);
+        return dfs(target, nums, memo);
+    }
+
+    public int dfs(int i, int[] nums, int[] memo) {
+        if (i == 0) {
+            return 1;
+        }
+        if (memo[i] != -1) {
+            return memo[i];
+        }
+        int res = 0;
+        for (int x : nums) {
+            if (i >= x) {
+                res += dfs(i - x, nums, memo);
+            }
+        }
+        return memo[i] = res;
+    }
+
+}
