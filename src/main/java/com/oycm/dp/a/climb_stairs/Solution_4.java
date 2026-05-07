0042a49890f1f7e222f333a07610190ca6bddb50
ouyangcm <mingorg@163.com>
2026-05-07 14:12:43 +0800

    爬楼梯-2466-类似转换

--- src/main/java/com/oycm/dp/a/climb_stairs/Solution_4.java
+++ src/main/java/com/oycm/dp/a/climb_stairs/Solution_4.java
@@ -1,14 +1,11 @@
 package com.oycm.dp.a.climb_stairs;
 
-import java.util.Arrays;
-
-
 public class Solution_4 {
 
     /**
      * 377. <a href="https://leetcode.cn/problems/combination-sum-iv/description/">组合总和 Ⅳ</a> 1600
      *
-     * @param nums 不同 整数组成的数组
+     * @param nums   不同 整数组成的数组
      * @param target 目标整数
      * @return 从 nums 中找出并返回总和为 target 的元素组合的个数。
      */
@@ -16,9 +13,18 @@ public class Solution_4 {
         /*
         题解思路：本质是 70 的爬楼梯，每次从 nums 中选一个数，作为往上爬的台阶数，计算爬 target 个台阶有多少种方案
          */
-        int[] memo = new int[target + 1];
-        Arrays.fill(memo, -1);
-        return dfs(target, nums, memo);
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= target; i++) {
+            for (int x : nums) {
+                if (x <= i) {
+                    dp[i] += dp[i - x];
+                }
+            }
+        }
+
+        return dp[target];
     }
 
     public int dfs(int i, int[] nums, int[] memo) {
---
+++ src/main/java/com/oycm/dp/a/climb_stairs/Solution_5.java
@@ -0,0 +1,88 @@
+package com.oycm.dp.a.climb_stairs;
+
+public class Solution_5 {
+
+    /**
+     * 2466. <a href="https://leetcode.cn/problems/count-ways-to-build-good-strings/description/">统计构造好字符串的方案数</a> 1694
+     *
+     * @param low
+     * @param high
+     * @param zero
+     * @param one
+     * @return
+     */
+    public int countGoodStrings(int low, int high, int zero, int one) {
+        /*
+        我们从空字符串开始构造一个字符串，每一步执行下面操作中的一种：
+            将 '0' 在字符串末尾添加 zero  次。
+            将 '1' 在字符串末尾添加 one 次。
+        以上操作可以执行任意次。
+        如果通过以上过程得到一个 长度 在 low 和 high 之间（包含上下边界）的字符串，那么这个字符串我们称为 好 字符串。
+        请你返回满足以上要求的 不同 好字符串数目。由于答案可能很大，请将结果对 1e9 + 7 取余 后返回。
+         */
+        /*
+        和 377 类似，可以转换成爬楼梯，两种步长爬楼梯，爬到 [low, high] 台阶的方案数
+        题解思路：f(i) 表示爬楼梯 zero, one 步的方案数
+         */
+        int[] dp = new int[high + 1];
+        int ans = 0;
+        dp[0] = 1;
+        for (int i = 1; i <= high; i++) {
+            if (i >= zero) dp[i] += dp[i - zero];
+            if (i >= one) dp[i] = (dp[i] + dp[i - one]) % mod;
+            if (i >= low) ans = (ans + dp[i]) % mod;
+        }
+        return ans;
+    }
+
+    public int dfs(int i, int zero, int one, int[] memo) {
+        /*
+        int ans = 0;
+        int[] memo = new int[high + 1];
+        Arrays.fill(memo, -1);
+        for (int i = low; i <= high; i++) {
+            ans = (ans + dfs(i, zero, one, memo)) % mod;
+        }
+
+        return ans;
+         */
+        if (i < 0) return 0;
+        if (i == 0) return 1;
+        if (memo[i] != -1) return memo[i];
+        return memo[i] = (dfs(i - zero, zero, one, memo) + dfs(i - one, zero, one, memo)) % mod;
+    }
+
+    public static final int mod = 1000000007;
+
+    /**
+     * dfs(0) 入口
+     *
+     * @param i
+     * @param low
+     * @param high
+     * @param nums
+     * @param memo
+     * @return
+     */
+    public int dfs(int i, int low, int high, int[] nums, int[] memo) {
+        /*
+        爬到 [low, high] 后续可能还可继续爬，后续的方案该如何记录
+         */
+        if (i > high) {
+            return 0;
+        }
+        if (memo[i] != -1) {
+            return memo[i];
+        }
+        long res = 0;
+        for (int num : nums) {
+            res = dfs(i + num, low, high, nums, memo) + res;
+            if (i + num >= low && i + num <= high) {
+                res++;
+            }
+        }
+
+        return memo[i] = (int) (res % mod);
+    }
+
+}
