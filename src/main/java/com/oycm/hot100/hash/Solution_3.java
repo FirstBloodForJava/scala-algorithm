1828d9e19f46e1820dda6f467c0813da8f476ac3
ouyangcm <mingorg@163.com>
2026-05-08 17:31:12 +0800

    hot100-hash-倍数连续

--- src/main/java/com/oycm/hot100/hash/Solution_3.java
+++ src/main/java/com/oycm/hot100/hash/Solution_3.java
@@ -1,7 +1,6 @@
 package com.oycm.hot100.hash;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Solution_3 {
 
@@ -91,6 +90,95 @@ public class Solution_3 {
     public static void main(String[] args) {
         Solution_3 solution = new Solution_3();
         System.out.println(solution.longestConsecutive(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}));
+    }
+
+}
+
+class Solution_2007 {
+
+    /**
+     * 2007. <a href="https://leetcode.cn/problems/find-original-array-from-doubled-array/description/">从双倍数组中还原原数组</a> 1557
+     *
+     * @param changed
+     * @return
+     */
+    public int[] findOriginalArray(int[] changed) {
+        /*
+        一个整数数组 original 可以转变成一个 双倍数组 changed ，转变方式为将 original 中每个元素 值乘以 2 加入数组中，然后将所有元素 随机打乱。
+        给你一个数组 changed ，如果 change 是 双倍 数组，那么请你返回 original数组，否则请返回空数组。original 的元素可以以 任意 顺序返回。
+         */
+        /*
+        changed.length % 2 == 0 长度要为偶数；
+        记录 nums 中元素 x 及次数
+        排序后，从左到右遍历 nums，x 可用次数 -1, 查找是否存在可用的 2x，不存在则返回空数组；存在则减少次数更新 ans
+        如果 x % 2 == 1，这个数只能作为 original 元素，看是否存在 2x 元素，不存在则返回 空数组
+         */
+        int n = changed.length;
+        if (n % 2 != 0) return new int[0];
+        Arrays.sort(changed);
+        int[] ans = new int[n / 2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int x : changed) {
+            map.merge(x, 1, Integer::sum);
+        }
+        int i = 0;
+        for (int x : changed) {
+            if (map.getOrDefault(x, 0) == 0) {
+                continue;
+            }
+            map.merge(x, -1, Integer::sum);
+            if (map.getOrDefault(2 * x, 0) == 0) {
+                return new int[0];
+            }
+            ans[i++] = x;
+            map.merge(2 * x, -1, Integer::sum);
+
+        }
+
+        return ans;
+    }
+
+    public int[] findOriginalArray_2(int[] changed) {
+        /*
+        changed.length % 2 == 0 长度要为偶数；
+        map 记录 nums 中元素 x 及次数
+
+        如果 x 为奇数，则必须作为 original 元素；
+        如果 x 为偶数，map 中 存在 x/2，不应该以 x 作为起点，寻找 original 元素
+        x, 2x 数量匹配 4x 存在则查看 8x 是否存在数量匹配
+         */
+
+        int n = changed.length;
+        if (n % 2 != 0) return new int[0];
+        int m = n / 2;
+        int[] ans = new int[m];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int x : changed) {
+            map.merge(x, 1, Integer::sum);
+        }
+        int i = 0;
+        for (Map.Entry<Integer, Integer> kv : map.entrySet()) {
+            // x 已配对
+            if (kv.getValue() == 0) {
+                continue;
+            }
+            int x = kv.getKey();
+            if (x % 2 == 1 || !map.containsKey(x / 2)) {
+                int next = x * 2;
+                // 数量不匹配
+                if (kv.getValue() > map.getOrDefault(next, 0)) {
+                    return new int[0];
+                }
+                // 数量清零
+                int cnt = kv.getValue();
+                kv.setValue(0);
+                map.merge(next, -cnt, Integer::sum);
+
+
+            }
+        }
+
+        return ans;
     }
 
 }
