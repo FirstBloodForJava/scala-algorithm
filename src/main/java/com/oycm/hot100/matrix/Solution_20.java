e3db1497be44de28b9b8be3d4c31ef0f7c1f5c4a
ouyangcm <mingorg@163.com>
2026-05-11 16:53:50 +0800

    hot100-20 矩阵翻转

---
+++ src/main/java/com/oycm/hot100/matrix/Solution_20.java
@@ -0,0 +1,25 @@
+package com.oycm.hot100.matrix;
+
+public class Solution_20 {
+
+    /**
+     * 48. <a href="https://leetcode.cn/problems/rotate-image/description/">旋转图像</a>
+     *
+     * @param matrix  n × n 的二维矩阵 matrix
+     */
+    public void rotate(int[][] matrix) {
+        /*
+        将图像顺时针旋转 90 度结果，原地修改
+         */
+        /*
+        横着看，第 i 行去了第 n-1-i 列
+        竖着看，第 j 列去了第 j 行
+        (i, j) => (j, n-1-i)
+        (i, j) => (j, i) => (j,n-1-i)
+        (i, j) => (j, i) 沿对角线翻转
+        (j, i) => (j,n-1-i) 行翻转
+         */
+
+    }
+
+}
