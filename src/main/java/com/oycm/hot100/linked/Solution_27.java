9876be966320d0224812432a8a0d140363cc6522
oycm <1164864987@qq.com>
2026-05-12 20:18:20 +0800

    hot100-27 合并两个有序链表，递归

--- src/main/java/com/oycm/hot100/linked/Solution_27.java
+++ src/main/java/com/oycm/hot100/linked/Solution_27.java
@@ -22,7 +22,24 @@ public class Solution_27 {
             否则，把 list2 插入到链表的尾节点，把 list2 替换成下一个节点
         循环结束后，如果由不为空的节点，添加到新链表的末尾
          */
+        /*
+        递归，头插法
+        merge(l1, l2)
+        分类讨论：
+            l1.val < l2.next，则等价于 l1.next = merge(l1.next, l2) 返回 l1
+            l1.val >= l2.next，则等价于 l2.next = merge(l1, l2.next) 返回 l2
+        递归边界：如果一个链表为空，返回另一个链表作为结果。
 
+         */
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
     }
 
 }
