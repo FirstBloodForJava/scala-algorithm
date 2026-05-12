cdb2ab5a300380cea8e01bb91d13c47c2d5e9477
oycm <1164864987@qq.com>
2026-05-12 21:59:09 +0800

    2140-解析分析补充

--- src/main/java/com/oycm/dp/a/rob/SolutionExtend_1.java
+++ src/main/java/com/oycm/dp/a/rob/SolutionExtend_1.java
@@ -15,7 +15,21 @@ public class SolutionExtend_1 {
         解决问题 i 将让你 获得  pointsi 的分数，但是你将 无法 解决接下来的 brainpoweri 个问题（即只能跳过接下来的 brainpoweri 个问题）。
         如果你跳过问题 i ，你可以对下一个问题决定使用哪种操作。
          */
-        //long ans = 0;
+        /*
+        原问题，对于 [0, n-1] 个问题，计算获得的最大分数
+        对于 questions[0]，分类讨论，选/不选
+            不选，计算 [1, n-1] 子问题的最大得分；
+            选，接下来 questions[0][1] 个问题不能选 [questions[0][1] + 1, n-1] 子问题的最大得分
+        dfs(i) = max(
+            dfs(i+1),
+            dfs(brainpoweri + i + 1) + pointsi
+        );
+        递归边界：i >= n 返回 0；
+        翻译成地推 dp[i] = max(
+            dp[i+1],
+            dp[j] + pointsi, j = min(n, i + brainpoweri + 1)
+        )
+         */
         int n = questions.length;
         long[] dp = new long[n + 1];
         dp[n - 1] = questions[n - 1][0];
