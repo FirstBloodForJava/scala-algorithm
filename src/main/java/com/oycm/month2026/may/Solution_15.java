0245277e8921fb813174d315af2a258ef6468a79
ouyangcm <mingorg@163.com>
2026-05-15 10:45:06 +0800

    每日一题-260515 二分查找，旋转数组最大值，开区间写法思路

--- src/main/java/com/oycm/month2026/may/Solution_15.java
+++ src/main/java/com/oycm/month2026/may/Solution_15.java
@@ -46,4 +46,47 @@ public class Solution_15 {
         return nums[r];
     }
 
+    public int findMax(int[] nums) {
+        /*
+        找出并返回数组中的 最大元素 。
+         */
+        /*
+        二分查找，x = nums[mid] 和最大值的位置关系
+        如果 x > nums[n-1]，说明：
+            数组分为了两个递增段；
+            第一段所有元素都大于第二段元素
+            x 在第一段；
+            最大值在第一段；
+            x 是最大值或在最大值的左边；
+        如果 x <= nums[n-1] 说明：
+            nums[n-1] >= nums[0] 说明是一段，最大值在 x 的右边，或者说 nums[n-1] 就是最大值
+            nums[n-1] < nums[0] 说明是两段，最大值在 x 的左边
+         */
+        /*
+        二分查找，x = nums[mid] 和 最大值的位置关系，和 nums[0] 比较大小
+        如果 x < nums[0]，说明：
+            数组分为两个递增段；
+            第一段所有元素都大于第二段元素
+            x 在第二段；
+            最大值在第一段；
+            x 在最大值的右边；
+        如果 x >= nums[0]，说明：
+            x 在第一段，或者没有分成两段
+            x 要么是最大值，要么在最大值的左边
+        因为返回的是 l，最大值可能就是 0，所以开区间左边要为 0，l 最大可为 n-1，右边为 1 即可
+         */
+        int n = nums.length;
+        int l = 0;
+        int r = n;
+        while (l + 1 < r) {
+            int mid = (l + r) >> 1;
+            if (nums[mid] < nums[0]) {
+                r = mid;
+            } else {
+                l = mid;
+            }
+        }
+        return nums[l];
+    }
+
 }
