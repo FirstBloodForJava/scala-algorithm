11bad54dda6bad37a7e6f013888d8ed6354de064
oycm <1164864987@qq.com>
2026-05-16 20:06:09 +0800

    hot100-55 全排列

---
+++ src/main/java/com/oycm/hot100/permutation/Solution_55.java
@@ -0,0 +1,25 @@
+package com.oycm.hot100.permutation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution_55 {
+
+    /**
+     * 46. <a href="https://leetcode.cn/problems/permutations/description/">全排列</a>
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        /*
+
+         */
+        List<List<Integer>> ans = new ArrayList<>();
+
+        return ans;
+    }
+
+
+
+}
