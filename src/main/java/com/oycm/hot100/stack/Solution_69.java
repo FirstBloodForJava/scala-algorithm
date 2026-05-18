9244d32743beffe610f90260e36e89d1aa8207b1
oycm <1164864987@qq.com>
2026-05-18 20:11:09 +0800

    hot100-69 栈

---
+++ src/main/java/com/oycm/hot100/stack/Solution_69.java
@@ -0,0 +1,20 @@
+package com.oycm.hot100.stack;
+
+import com.oycm.datastructure.stack.rbs.Solution_1;
+
+public class Solution_69 {
+
+    /**
+     * 20. <a href="https://leetcode.cn/problems/valid-parentheses/description/">有效的括号</a>
+     *
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        /*
+
+         */
+        return new Solution_1().isValid(s);
+    }
+
+}
