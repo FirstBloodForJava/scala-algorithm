37c54dd234257f9ba12ac911c4547a881c8731ce
ouyangcm <mingorg@163.com>
2026-05-19 09:20:31 +0800

    2023-dualweek-98-1 双指针

---
+++ src/main/java/com/oycm/dualweek2023/No98/Solution_1.java
@@ -0,0 +1,19 @@
+package com.oycm.dualweek2023.No98;
+
+
+import com.oycm.month2026.may.Solution_19;
+
+public class Solution_1 {
+
+    /**
+     * 2540. <a href="https://leetcode.cn/problems/minimum-common-value/description/">最小公共值</a> 1250
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int getCommon(int[] nums1, int[] nums2) {
+        return new Solution_19().getCommon(nums1, nums2);
+    }
+
+}
