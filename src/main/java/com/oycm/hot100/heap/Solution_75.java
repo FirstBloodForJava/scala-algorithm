f06c66e189a9287bf070117dde0c8241c678bb35
ouyangcm <mingorg@163.com>
2026-05-20 11:22:26 +0800

    hot100-75 桶排序

--- src/main/java/com/oycm/hot100/heap/Solution_75.java
+++ src/main/java/com/oycm/hot100/heap/Solution_75.java
@@ -1,8 +1,6 @@
 package com.oycm.hot100.heap;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Solution_75 {
 
@@ -33,6 +31,32 @@ public class Solution_75 {
         while (k-- > 0) {
             ans[i++] = max.poll()[1];
         }
+        return ans;
+    }
+
+    public int[] bucketSort(int[] nums, int k) {
+        /*
+        先记录数组中相同元素出现频次情况，同时记录一个元素出现次数最大值 maxCnt
+        创建一个桶，长度为 maxCnt，遍历 map，把 key 次数作为桶的下标，加入到桶中
+        倒序遍历这个桶，由于题目保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的，顺序可以无要求
+         */
+        Map<Integer, Integer> cnt = new HashMap<>();
+        for (int num : nums) {
+            cnt.merge(num, 1, Integer::sum);
+        }
+        int maxCnt = Collections.max(cnt.values());
+        List<Integer>[] buckets = new List[maxCnt + 1];
+        int[] ans = new int[k];
+        Arrays.setAll(buckets, l -> new ArrayList<>());
+        for (Map.Entry<Integer, Integer> entry : cnt.entrySet()) {
+            buckets[entry.getValue()].add(entry.getKey());
+        }
+        for (int i = maxCnt, j = 0; j < k; i--) {
+            for (int val : buckets[i]) {
+                ans[j++] = val;
+            }
+        }
+
         return ans;
     }
 
