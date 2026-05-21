a20f6aacae46dc08e24c075920255662af49a2e3
oycm <1164864987@qq.com>
2026-05-21 22:39:19 +0800

    hot100-84 选/不选 可重复选，求最少次数

---
+++ src/main/java/com/oycm/hot100/dp/Solution_84.java
@@ -0,0 +1,45 @@
+package com.oycm.hot100.dp;
+
+import java.util.Arrays;
+
+public class Solution_84 {
+
+    /**
+     * 279.
+     * <a href="https://leetcode.cn/problems/perfect-squares/description/">完全平方数</a>
+     *
+     * @param n
+     * @return
+     */
+    public int numSquares(int n) {
+        /*
+        给你一个整数 n ，返回 和为 n 的完全平方数的最少数量。
+        完全平方数 是一个整数，其值等于另一个整数的平方；换句话说，其值等于一个整数自乘的积。
+         */
+        /*
+        题解思路：定义 dfs(i, j) 表示从前 i 个完全平方数中选一些数（可以重复选），满足元素和恰好为 j 的最少选择次数。
+        考虑第 i 个完全平方数 i*i 选/不选：
+            不选，从前 i-1 个完全平方数中选一些数（可以重复选），满足元素和恰好为 j 的最少选择次数。
+            选，从前 i 个完全平方数中选一些数（可以重复选），满足元素和恰好为 j-i*i 的最少选择次数。
+         */
+        return dfs((int) Math.sqrt(n), n);
+    }
+
+    static int[][] memo = new int[101][10001];
+
+    static {
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+    }
+
+    public int dfs(int i, int j) {
+        if (i == 0) return j == 0 ? 0 : Integer.MAX_VALUE;
+        if (memo[i][j] != -1) return memo[i][j];
+        // 不能选
+        if (j < i * i) return memo[i][j] = dfs(i - 1, j);
+
+        return memo[i][j] = Math.min(dfs(i - 1, j), dfs(i, j - i * i) + 1);
+    }
+
+}
