cdfa30230bd9cb0662eac09a44ad5927db98657e
oycm <1164864987@qq.com>
2026-05-23 10:19:11 +0800

    dp 包结构说明

---
+++ src/main/java/com/oycm/dp/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * 动态规划
+ * <p>a. 入门 dp</p>
+ * <ol>
+ *     <li>climb_stairs 爬楼梯</li>
+ *     <li>rob 打家劫舍</li>
+ *     <li>sub_array_sum 最大子数组和</li>
+ * </ol>
+ *
+ * <p>b. 网格图 dp</p>
+ * <ol>
+ *     <li></li>
+ * </ol>
+ *
+ * <p>c. 背包</p>
+ * <ol>
+ *     <li>backpack 0-1 背包</li>
+ *     <li>complete_backpack 完全背包</li>
+ *     <li></li>
+ * </ol>
+ */
+package com.oycm.dp;
