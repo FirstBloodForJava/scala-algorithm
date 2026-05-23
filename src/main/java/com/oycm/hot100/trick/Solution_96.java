375751beeafb9ac5c020ff20e077f87647bae47e
oycm <1164864987@qq.com>
2026-05-23 22:11:06 +0800

    文件名调整

--- src/main/java/com/oycm/hot100/trick/Solution_96.java
+++ src/main/java/com/oycm/hot100/trick/Solution_96.java
@@ -1,24 +1,17 @@
 package com.oycm.hot100.trick;
 
-import com.oycm.algorithm.h.basic.Solution_22;
+import com.oycm.algorithm.h.other.Solution_1;
 
 public class Solution_96 {
 
     /**
-     * 169. <a href="https://leetcode.cn/problems/majority-element/description/">多数元素</a>
+     * 136. <a href="https://leetcode.cn/problems/single-number/description/">只出现一次的数字</a>
      *
-     * @param nums
-     * @return 在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素
+     * @param nums nums 中其他元素都出现了 2 次
+     * @return 找出只出现一次数字的值
      */
-    public int majorityElement(int[] nums) {
-        /*
-        打擂台，初始化 ans = nums[0]，擂主血量为 0；
-        遍历数组：
-            如果擂主血量为 0，则下一位上台者为擂主，且血量 +1；
-            否则，判断当前选水是否就是擂主，不是则血量 -1；是则血量 +1；
-            最后 擂主就是出现次数大于 n/2 次数的元素
-         */
-        return new Solution_22().majorityElement(nums);
+    public int singleNumber(int[] nums) {
+        return new Solution_1().singleNumber(nums);
     }
 
 }
