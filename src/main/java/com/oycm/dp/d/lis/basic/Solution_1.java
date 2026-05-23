b2769047ceeef3f6e20d537151bd1142a2ade243
oycm <1164864987@qq.com>
2026-05-23 19:53:41 +0800

    lis 贪心+二分查找

--- src/main/java/com/oycm/dp/d/lis/basic/Solution_1.java
+++ src/main/java/com/oycm/dp/d/lis/basic/Solution_1.java
@@ -64,3 +64,42 @@ class Solution_300_1 {
         return ans;
     }
 }
+
+class Solution_300_2 {
+
+
+    public int lengthOfLIS(int[] nums) {
+        /*
+        定义数组 g[i] 表示长度为 i+1 子序列末尾元素的最小值。
+        g[i] 是严格递增的。
+            如果 g[i] >= g[j] 且 i < j，那么 g[j] 对应子序列第 i 个数应该 小于 g[j]，与 g[i] >= g[j] 矛盾。
+        一次只能更新个位置
+            如果更新两个位置，g 中就存在两个相同的元素，导致 g 不是严格递增的。
+        更新的位置，是第一个大于等于 nums[i] 数的下标，如果 nums[i] 比 g 的最后一个数都大，则添加到末尾。
+         */
+        int gn = 0;
+        for (int x : nums) {
+            int j = lowerBound(nums, gn, x);
+            nums[j] = x;
+            if (j == gn) {
+                gn++;
+            }
+        }
+
+        return gn;
+    }
+
+    public int lowerBound(int[] nums, int r, int target) {
+        int l = -1;
+        while (l + 1 < r) {
+            int mid = (l + r) >> 1;
+            if (nums[mid] < target){
+                l = mid;
+            } else {
+                r = mid;
+            }
+        }
+        return r;
+    }
+
+}
