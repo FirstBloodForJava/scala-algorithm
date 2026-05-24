dc7026e30c2d12203d1699c795673b37301ce70a
oycm <1164864987@qq.com>
2026-05-24 20:01:15 +0800

    hot100-95 一次从中间枚举

--- src/main/java/com/oycm/hot100/dp_mult/Solution_93.java
+++ src/main/java/com/oycm/hot100/dp_mult/Solution_93.java
@@ -13,32 +13,41 @@ public class Solution_93 {
         给你一个字符串 s，找到 s 中最长的 回文 子串。
          */
         /*
-        枚举 s i 从 [0, n-1], j 从 [n-1] j > i, 判断 [i, j] 是否为回文字段
+        暴力：枚举 s i 从 [0, n-1], j 从 [n-1] j > i, 判断 [i, j] 是否为回文字段
+         */
+        /*
+        从中间向两端枚举回文串：
+            abcab a 为中心的子串，判断左右两边是否相等，相等，则继续向外扩展；不相等，则开始下一个中心点枚举
+        有两种情况，回文串长度是奇数或偶数
+            奇数枚举 l = r = i，l--, r++；循环结束时 [l+1, r-1] 这段就是回文串，长度为 r-l-1
+            偶数枚举 l = i, r = r + 1; 要先判断 是否越界，以及 cs[l] 是否等于 cs[r]
+        合并一个循环枚举：
+            枚举 0, 1, 2, 3, ... 2n - 2
+        当 i 是偶数时，枚举奇数长度中心；l = i/2 r= (i+1)/2 l 和 r 恰好是相等的
+        当 i 时奇数时，枚举偶数长度中心；l = i/2 r= (i+1)/2 r 恰好比 l 多 1
          */
         int n = s.length();
         int mx = 0;
-        String ans = null;
+        int left = 0, right = 0;
         char[] cs = s.toCharArray();
-        for (int i = 0; i < n; i++) {
-            for (int j = n - 1; j >= i; j--) {
-                int l = i;
-                int r = j;
-                boolean isPalindrome = true;
-                while (l <= r && isPalindrome) {
-                    if (cs[l] != cs[r]) {
-                        isPalindrome = false;
-                    }
-                    l++;
-                    r--;
-                }
-                if (isPalindrome && j - i + 1 > mx) {
-                    mx = j - i + 1;
-                    ans = s.substring(i, j + 1);
-                }
+
+        // 合并枚举
+        for (int i = 0; i < 2 * n - 1; i++) {
+            int l = i / 2;
+            int r = (i + 1) >> 1;
+            while (l >= 0 && r < n && cs[l] == cs[r]) {
+                l--;
+                r++;
             }
+            if (r - l - 1 > mx) {
+                mx = r - l - 1;
+                left = l + 1;
+                right = r;
+            }
+
         }
 
-        return ans;
+        return s.substring(left, right);
     }
 
 }
