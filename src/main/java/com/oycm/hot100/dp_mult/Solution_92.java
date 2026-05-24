f0347d396977007e1c2f6e958785c1a3a5884dad
oycm <1164864987@qq.com>
2026-05-24 16:58:09 +0800

    hot100-92 网格图最短路径和 dp

--- src/main/java/com/oycm/hot100/dp_mult/Solution_92.java
+++ src/main/java/com/oycm/hot100/dp_mult/Solution_92.java
@@ -33,3 +33,36 @@ public class Solution_92 {
 
 
 }
+
+class Solution_64 {
+
+    public int minPathSum(int[][] grid) {
+        /*
+        dfs(i, j) = min(dfs(i-1, j), dfs(i, j-1)) + grid[i][j]
+        f[i][j] = min(f[i-1][j], f[i][j-1]) + grid[i][j];
+        f[i+1][j+1] = min(f[i][j+1], f[i+1][j]) + grid[i][j];
+        f[0][j], j [1, n-1] 初始化成 Integer.Max_VALUE，表示无效路径
+        f[i][0], i [1, m-1] 初始化成 Integer.Max_VALUE，表示无效路径
+        才能使用直接使用上面的判断，
+            如果 i = 0 f[i+1][j+1] = grid[i][j] + f[i+1][j];
+            如果 j = 0 f[i+1][j+1] = grid[i][j] + f[i][j+1];
+         */
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] f = new int[m + 1][n + 1];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0) {
+                    f[i + 1][j + 1] = grid[i][j] + f[i + 1][j];
+                } else if (j == 0) {
+                    f[i + 1][j + 1] = grid[i][j] + f[i][j + 1];
+                } else {
+                    f[i + 1][j + 1] = Math.min(f[i][j + 1], f[i + 1][j]) + grid[i][j];
+                }
+            }
+        }
+
+        return f[m][n];
+    }
+}
