c0f696371763cbf2d4a57879f8b25dbcf9aec468
ouyangcm <mingorg@163.com>
2026-05-29 11:24:53 +0800

    2023-week-332-4 前后缀分解+子序列

---
+++ src/main/java/com/oycm/dualweek2024/No140/Solution_3.java
@@ -0,0 +1,26 @@
+package com.oycm.dualweek2024.No140;
+
+public class Solution_3 {
+
+    /**
+     * 3302. <a href="https://leetcode.cn/problems/find-the-lexicographically-smallest-valid-sequence/description/">字典序最小的合法序列</a> 2474
+     *
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public int[] validSequence(String word1, String word2) {
+        /*
+        给你两个字符串 word1 和 word2。
+        如果一个字符串 x 修改 至多 一个字符会变成 y ，那么我们称它与 y 几乎相等。
+        如果一个下标序列 seq 满足以下条件，我们称它是 合法的：
+            下标序列是 升序 的。
+            将 word1 中这些下标对应的字符 按顺序 连接，得到一个与 word2 几乎相等 的字符串。
+            请你返回一个长度为 word2.length 的数组，表示一个 字典序最小 的 合法 下标序列。如果不存在这样的序列，请你返回一个 空 数组。
+        注意 ，答案数组必须是字典序最小的下标数组，而 不是 由这些下标连接形成的字符串。
+        1 <= word2.length < word1.length <= 3 * 1e5
+         */
+        return null;
+    }
+
+}
