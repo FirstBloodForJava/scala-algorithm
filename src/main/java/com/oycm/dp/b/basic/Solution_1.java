afb4c649eac326bea83870cabe0930d8c873ed58
oycm <1164864987@qq.com>
2026-05-31 20:32:58 +0800

    网格图 dp 原地修改

--- src/main/java/com/oycm/dp/b/basic/Solution_1.java
+++ src/main/java/com/oycm/dp/b/basic/Solution_1.java
@@ -27,7 +27,7 @@ public class Solution_1 {
         for (int[] row : memo) {
             Arrays.fill(row, -1);
         }
-        return dfs(m-1, n-1, grid, memo);
+        return dfs(m - 1, n - 1, grid, memo);
     }
 
     public int dfs(int i, int j, int[][] grid, int[][] memo) {
@@ -37,4 +37,40 @@ public class Solution_1 {
         return memo[i][j] = Math.min(dfs(i, j - 1, grid, memo), dfs(i - 1, j, grid, memo)) + grid[i][j];
     }
 
+    public int minPathSum_dp(int[][] grid) {
+        /*
+        dfs(i, j) = min(dfs(i-1, j), dfs(i, j-1)) + grid[i][j];
+        翻译成递推
+        f[i][j] = min(f[i-1][j], f[i][j-1]) + grid[i][j]
+        为了避免负数下标，f[i+1][j+1] 表示 (0, 0) 到 (i, j) 最小路径和
+        f[i+1][j+1] = min(f[i][j+1], f[i+1][j]) + grid[i][j], f[m][n] 就是答案，
+        初始化 f[m+1][n+1]
+        第一行 f[0] = Integer.MAX_VALUE; 第一列 f[i][0] = Integer.MAX_VALUE
+         */
+        /*
+        空间优化：
+        计算 (0, 0) 到 (i, j) 的最小路径和是，只会用到 f[i][j+1] 和 f[i+1][j]。
+            f[i][j+1] 是上一行当前列，
+            f[i+1][j] 是当前行前一列，
+            直接把这些合并到一行记录，当前位置左边 f[i+1][j]，即 f[j]，上一行当前列 f[i][j+1]，即 f[j+1]
+            f[j+1] = min(f[j], f[j+1]) + row[j]
+         */
+        /*
+        原地修改：
+        f[i][j] = min(f[i-1][j], f[i][j-1]) + grid[i][j]
+        需要特殊处理 i = 0 或 j = 0 特殊行和特殊列
+         */
+        int m = grid.length, n = grid[0].length;
+        int[] f = grid[0];
+        for (int j = 1; j < n; j++) {
+            f[j] += f[j - 1];
+        }
+        for (int i = 1; i < m; i++) {
+            f[0] += grid[i][0];
+            for (int j = 1; j < n; j++) {
+                f[j] = Math.min(f[j], f[j - 1]) + grid[i][j];
+            }
+        }
+        return f[n - 1];
+    }
 }
