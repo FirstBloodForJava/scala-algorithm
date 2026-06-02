c6cf61538d0ba6644883e62e4bf53e6a8a2d3fcf
oycm <1164864987@qq.com>
2026-06-02 22:25:32 +0800

    dp 比 dfs 慢

--- src/main/java/com/oycm/dp/b/basic/Solution_6.java
+++ src/main/java/com/oycm/dp/b/basic/Solution_6.java
@@ -24,11 +24,7 @@ public class Solution_6 {
         /*
         定义 dfs(i, j) 表示从 (i, j) 到达最后一行的路径最小和，
             拆分成子问题 (i+1, j-1), (i+1, j), (i+1, j+1)
-        dfs(i, j) = min(
-            dfs(i+1, j-1),
-            dfs(i+1, j),
-            dfs(i+1, j+1)
-        ) + matrix[i][j]
+
          */
         int n = matrix.length;
         int[][] memo = new int[n][n];
@@ -48,6 +44,45 @@ public class Solution_6 {
         if (memo[i][j] != Integer.MAX_VALUE) return memo[i][j];
         return memo[i][j] = matrix[i][j] + Math.min(Math.min(dfs(i + 1, j - 1, matrix, memo), dfs(i + 1, j + 1, matrix, memo)),
                 dfs(i + 1, j, matrix, memo));
+    }
+
+    public int minFallingPathSum_dp(int[][] matrix) {
+        /*
+        自顶到底的 dfs 转换成 递推，不是很好写，转换成自底到顶
+        dfs(i, j) = min(
+            dfs(i-1, j-1),
+            dfs(i-1, j),
+            dfs(i-1, j+1)
+        ) + matrix[i][j]
+        递归边界
+            j < 0 || j >= n，返回无穷大
+            i == 0, 返回 matrix[i][j]
+        翻译成递推：
+        f[i][j] = min(
+            f[i-1][j-1],
+            f[i-1][j],
+            f[i-1][j+1]
+        ) + matrix[i][j]
+        为了避免负数下标，f[i][j] 定义为 f[i][j+1]
+        空间优化，计算 f[i][j+1] 只需要三个变量，上一行当前位置 左中右，中间会覆盖，这一行又会只有左边的数据，所有需要一个临时变量记录左边的数据
+         */
+        int n = matrix.length;
+        int[] f = new int[n + 2];
+        System.arraycopy(matrix[0], 0, f, 1, n);
+        f[0] = f[n + 1] = Integer.MAX_VALUE;
+        for (int i = 1; i < n; i++) {
+            int pre = f[0];
+            for (int j = 0; j < n; j++) {
+                int tmp = pre;
+                pre = f[j + 1];
+                f[j + 1] = Math.min(Math.min(tmp, f[j + 2]), f[j + 1]) + matrix[i][j];
+            }
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int i = 1; i <= n; i++) {
+            ans = Math.min(ans, f[i]);
+        }
+        return ans;
     }
 
 }
