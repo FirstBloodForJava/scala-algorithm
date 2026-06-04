e09ddfff78d83519ef16b89df2ab7a8f3301a381
ouyangcm <mingorg@163.com>
2026-06-04 16:22:07 +0800

    2025-dualweek-170-4 数位 dp todo

---
+++ src/main/java/com/oycm/dualweek2025/No170/Solution_4.java
@@ -0,0 +1,28 @@
+package com.oycm.dualweek2025.No170;
+
+public class Solution_4 {
+
+    /**
+     * 3753. <a href="https://leetcode.cn/problems/total-waviness-of-numbers-in-range-ii/description/">范围内总波动值 II</a> 2297
+     *
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public long totalWaviness(long num1, long num2) {
+        /*
+        给你两个整数 num1 和 num2，表示一个 闭 区间 [num1, num2]。
+        一个数字的 波动值 定义为该数字中 峰 和 谷 的总数：
+            如果一个数位 严格大于 其两个相邻数位，则该数位为 峰。
+            如果一个数位 严格小于 其两个相邻数位，则该数位为 谷。
+            数字的第一个和最后一个数位 不能 是峰或谷。
+            任何少于 3 位的数字，其波动值均为 0。
+        返回范围 [num1, num2] 内所有数字的波动值之和。
+         */
+        /*
+        1 <= num1 <= num2 <= 1e15
+         */
+        return 0;
+    }
+
+}
