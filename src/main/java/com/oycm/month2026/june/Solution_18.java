4b94dccea7978ab7e76640e4ce2799a397d95749
ouyangcm <mingorg@163.com>
2026-06-18 15:38:29 +0800

    每日一题-260618 数学公式

---
+++ src/main/java/com/oycm/month2026/june/Solution_18.java
@@ -0,0 +1,23 @@
+package com.oycm.month2026.june;
+
+import com.oycm.dualweek2020.No19.Solution_3;
+
+public class Solution_18 {
+
+    /**
+     * 1344.
+     * <a href="https://leetcode.cn/problems/angle-between-hands-of-a-clock/description/">时钟指针的夹角</a> 1325
+     *
+     * @param hour    [1, 12]
+     * @param minutes [0, 59]
+     * @return
+     */
+    public double angleClock(int hour, int minutes) {
+        /*
+        给你两个数 hour 和 minutes 。请你返回在时钟上，由给定时间的时针和分针组成的较小角的角度（60 单位制）。
+         */
+
+        return new Solution_3().angleClock(hour, minutes);
+    }
+
+}
