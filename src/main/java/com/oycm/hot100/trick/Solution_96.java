b0aa35462f4077326d4c6ab4a6df6d199568a700
ouyangcm <mingorg@163.com>
2026-06-24 16:59:59 +0800

    只出现一次的数字

--- src/main/java/com/oycm/hot100/trick/Solution_96.java
+++ src/main/java/com/oycm/hot100/trick/Solution_96.java
@@ -1,7 +1,5 @@
 package com.oycm.hot100.trick;
 
-import com.oycm.algorithm.h.other.Solution_1;
-
 public class Solution_96 {
 
     /**
@@ -11,7 +9,19 @@ public class Solution_96 {
      * @return 找出只出现一次数字的值
      */
     public int singleNumber(int[] nums) {
-        return new Solution_1().singleNumber(nums);
+        /*
+        给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+        你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。
+         */
+        /*
+        利用 a ^ a = 0，a ^ 0 = a 异或运算性质
+         */
+        int ans = 0;
+        for (int x : nums) {
+            ans ^= x;
+        }
+
+        return ans;
     }
 
 }
