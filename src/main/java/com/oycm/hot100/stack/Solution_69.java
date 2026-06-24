69a7415a2dc79b48f6639fc57b0b70c1c54ef71e
ouyangcm <mingorg@163.com>
2026-06-24 16:26:07 +0800

    有效的括号

--- src/main/java/com/oycm/hot100/stack/Solution_69.java
+++ src/main/java/com/oycm/hot100/stack/Solution_69.java
@@ -1,6 +1,9 @@
 package com.oycm.hot100.stack;
 
-import com.oycm.datastructure.stack.rbs.Solution_1;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution_69 {
 
@@ -12,9 +15,28 @@ public class Solution_69 {
      */
     public boolean isValid(String s) {
         /*
-
+        给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+        有效字符串需满足：
+            左括号必须用相同类型的右括号闭合。
+            左括号必须以正确的顺序闭合。
+            每个右括号都有一个对应的相同类型的左括号。
          */
-        return new Solution_1().isValid(s);
+        /*
+        (, {, [ 入栈，如果匹配到 ), }, ]，对应的栈顶元素要是对应的左括号
+         */
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+        Deque<Character> st = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (!map.containsKey(c)) {
+                st.push(c);
+            } else {
+                if (st.isEmpty() || map.get(c) != st.pop()) return false;
+            }
+        }
+        return st.isEmpty();
     }
 
 }
