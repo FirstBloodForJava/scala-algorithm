78cbabf81157c44c0e951be63b2fbd3d7a3b1f75
ouyangcm <mingorg@163.com>
2026-06-25 16:24:46 +0800

    回溯-括号生成-选/不选

--- src/main/java/com/oycm/hot100/permutation/Solution_59.java
+++ src/main/java/com/oycm/hot100/permutation/Solution_59.java
@@ -27,7 +27,7 @@ public class Solution_59 {
         return ans;
     }
 
-    public void dfs(int i, int balnace, int n, List<Integer> path, List<String> ans) {
+    public void dfs(int i, int balance, int n, List<Integer> path, List<String> ans) {
         if (path.size() == n) {
             char[] cs = new char[n * 2];
             Arrays.fill(cs, ')');
@@ -37,16 +37,39 @@ public class Solution_59 {
             ans.add(new String(cs));
             return;
         }
-        for (int right = 0; right <= balnace ; right++) {
+        for (int right = 0; right <= balance; right++) {
             // 先填 right 个右括号，再填一个左括号， i+right 表示当前填的左括号下标
             // (i, i+right) 直接填右括号
             path.add(i + right);
-            dfs(i + right + 1, balnace - right + 1, n, path, ans);
+            dfs(i + right + 1, balance - right + 1, n, path, ans);
             // 回溯
             path.remove(path.size() - 1);
         }
     }
 
+    public List<String> generateParenthesis_1(int n) {
+        List<String> ans = new ArrayList<>();
+        char[] path = new char[2 * n];
+        dfs(0, 0, n, path, ans);
+        return ans;
+    }
+
+    public void dfs(int left, int right, int n, char[] path, List<String> ans) {
+        if (right == n) {
+            ans.add(new String(path));
+            return;
+        }
+        // 选 (
+        if (left < n) {
+            path[left + right] = '(';
+            dfs(left + 1, right, n, path, ans);
+        }
+        // 不选 (，选 )
+        if (right < left) {
+            path[left + right] = ')';
+            dfs(left, right + 1, n, path, ans);
+        }
+    }
 
 
 }
