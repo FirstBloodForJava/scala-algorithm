5f79375f4797b703759447e0b7185335fc5f154f
oycm <1164864987@qq.com>
2026-07-02 20:28:38 +0800

    AC 自动机题单

---
+++ src/main/java/com/oycm/string/ac_automaton/Solution_1.java
@@ -0,0 +1,31 @@
+package com.oycm.string.ac_automaton;
+
+public class Solution_1 {
+}
+
+/**
+ * 1032. 字符流
+ * <br>
+ * 1032. <a href="https://leetcode.cn/problems/stream-of-characters/description/">字符流</a> 1970
+ *
+ */
+class StreamChecker {
+    /*
+    设计一个算法：接收一个字符流，并检查这些字符的后缀是否是字符串数组 words 中的一个字符串。
+    例如，words = ["abc", "xyz"] 且字符流中逐个依次加入 4 个字符 'a'、'x'、'y' 和 'z' ，
+    你所设计的算法应当可以检测到 "axyz" 的后缀 "xyz" 与 words 中的字符串 "xyz" 匹配。
+     */
+
+    public StreamChecker(String[] words) {
+        /*
+        构造函数，用字符串数组 words 初始化数据结构。
+         */
+    }
+
+    public boolean query(char letter) {
+        /*
+        从字符流中接收一个新字符，如果字符流中的任一非空后缀能匹配 words 中的某一字符串，返回 true ；否则，返回 false。
+         */
+        return false;
+    }
+}
---
+++ src/main/java/com/oycm/string/ac_automaton/Solution_2.java
@@ -0,0 +1,22 @@
+package com.oycm.string.ac_automaton;
+
+public class Solution_2 {
+
+    /**
+     * 1967. 作为子字符串出现在单词中的字符串数目
+     * <br>
+     * 1967. <a href="https://leetcode.cn/problems/number-of-strings-that-appear-as-substrings-in-word/description/">作为子字符串出现在单词中的字符串数目</a> 1232
+     *
+     * @param patterns
+     * @param word
+     * @return
+     */
+    public int numOfStrings(String[] patterns, String word) {
+        /*
+        给你一个字符串数组 patterns 和一个字符串 word ，统计 patterns 中有多少个字符串是 word 的子字符串。返回字符串数目。
+        子字符串 是字符串中的一个连续字符序列。
+         */
+        return 0;
+    }
+
+}
