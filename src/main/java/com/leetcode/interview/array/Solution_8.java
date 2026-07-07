7fc009d30c9fa2263733ca01b1c451b4a836a06e
oycm <1164864987@qq.com>
2026-07-07 22:25:14 +0800

    lis

---
+++ src/main/java/com/leetcode/interview/array/Solution_8.java
@@ -0,0 +1,23 @@
+package com.leetcode.interview.array;
+
+public class Solution_8 {
+
+    /**
+     * 334. 递增的三元子序列
+     * <br>
+     * 334. <a href="https://leetcode.cn/problems/increasing-triplet-subsequence/description/">递增的三元子序列</a>
+     *
+     * @param nums
+     * @return
+     */
+    public boolean increasingTriplet(int[] nums) {
+        /*
+        给你一个整数数组 nums ，判断这个数组中是否存在长度为 3 的 递增子序列。
+        如果存在这样的三元组下标 (i, j, k) 且满足 i < j < k ，使得 nums[i] < nums[j] < nums[k] ，返回 true ；否则，返回 false 。
+         */
+        /*
+        递增子序列的子序列一定是递增的，求最长递增子序列的过程，判断子序列长度是否大于 3
+         */
+        return false;
+    }
+}
