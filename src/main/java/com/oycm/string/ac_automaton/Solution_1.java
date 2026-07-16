c8ba36bd0df64137845d1bc8739a3ea0f5be1552
ouyangcm <mingorg@163.com>
2026-07-16 11:17:33 +0800

    ac 自动机，查找后缀是否匹配模式串

--- src/main/java/com/oycm/string/ac_automaton/Solution_1.java
+++ src/main/java/com/oycm/string/ac_automaton/Solution_1.java
@@ -7,7 +7,6 @@ public class Solution_1 {
  * 1032. 字符流
  * <br>
  * 1032. <a href="https://leetcode.cn/problems/stream-of-characters/description/">字符流</a> 1970
- *
  */
 class StreamChecker {
     /*
@@ -16,16 +15,32 @@ class StreamChecker {
     你所设计的算法应当可以检测到 "axyz" 的后缀 "xyz" 与 words 中的字符串 "xyz" 匹配。
      */
 
+    AhoCorasick ac;
+    Node cur;
+
     public StreamChecker(String[] words) {
         /*
         构造函数，用字符串数组 words 初始化数据结构。
          */
+
+        ac = new AhoCorasick();
+        for (String pattern : words) {
+            ac.put(pattern);
+        }
+        ac.buildFail();
+        cur = ac.root;
     }
 
     public boolean query(char letter) {
         /*
         从字符流中接收一个新字符，如果字符流中的任一非空后缀能匹配 words 中的某一字符串，返回 true ；否则，返回 false。
          */
+        cur = cur.son[letter - 'a'];
+        if (cur.cnt > 0) return true;
+        for (Node node = cur.last; node != ac.root; node = node.last) {
+            if (node.cnt > 0) return true;
+        }
+
         return false;
     }
 }
