28efeed1f9c7678ec6955be20e2039ab60e51a1e
ouyangcm <mingorg@163.com>
2026-07-23 17:30:02 +0800

    完全背包-构造

---
+++ src/main/java/com/oycm/dp/c/complete_backpack/SolutionExtend_1.java
@@ -0,0 +1,26 @@
+package com.oycm.dp.c.complete_backpack;
+
+import java.util.List;
+
+public class SolutionExtend_1 {
+
+    /**
+     * 3592. <a href="https://leetcode.cn/problems/inverse-coin-change/description/">硬币面值还原</a> 1701
+     *
+     * @param numWays
+     * @return
+     */
+    public List<Integer> findCoins(int[] numWays) {
+        /*
+        给你一个 从 1 开始计数 的整数数组 numWays，其中 numWays[i] 表示使用某些 固定 面值的硬币（每种面值可以使用无限次）凑出总金额 i 的方法数。
+        每种面值都是一个 正整数 ，并且其值 最多 为 numWays.length。
+        然而，具体的硬币面值已经 丢失 。你的任务是还原出可能生成这个 numWays 数组的面值集合。
+        返回一个按从小到大顺序排列的数组，其中包含所有可能的 唯一 整数面值。
+        如果不存在这样的集合，返回一个 空 数组。
+         */
+        /*
+
+         */
+        return null;
+    }
+}
