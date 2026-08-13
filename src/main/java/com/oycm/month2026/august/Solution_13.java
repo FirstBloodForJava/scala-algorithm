03cb1a573cf59399ab2f96dc5c611e4b0ff59c8c
ouyangcm <mingorg@163.com>
2026-08-13 17:25:31 +0800

    每日一题-260813

---
+++ src/main/java/com/oycm/month2026/august/Solution_13.java
@@ -0,0 +1,24 @@
+package com.oycm.month2026.august;
+
+public class Solution_13 {
+
+    /**
+     * 2213. <a href="https://leetcode.cn/problems/longest-substring-of-one-repeating-character/description/">由单个字符重复的最长子字符串</a> 2629
+     *
+     * @param s
+     * @param queryCharacters
+     * @param queryIndices
+     * @return
+     */
+    public int[] longestRepeating(String s, String queryCharacters, int[] queryIndices) {
+        /*
+        给你一个下标从 0 开始的字符串 s。
+        另给你一个下标从 0 开始、长度为 k 的字符串 queryCharacters，一个下标从 0 开始、长度也是 k 的整数 下标 数组 queryIndices ，这两个都用来描述 k 个查询。
+        第 i 个查询会将 s 中位于下标 queryIndices[i] 的字符更新为 queryCharacters[i] 。
+        返回一个长度为 k 的数组 lengths ，其中 lengths[i] 是在执行第 i 个查询 之后 s 中仅由 单个字符重复 组成的 最长子字符串 的 长度 。
+         */
+
+        return null;
+    }
+
+}
