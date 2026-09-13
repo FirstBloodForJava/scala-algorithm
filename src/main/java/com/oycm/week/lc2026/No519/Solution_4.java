b6dd9eb60e354869d35c030729037ad1ea1b5bee
oycm <1164864987@qq.com>
2026-09-13 17:44:20 +0800

    No518-4: todo(值域分治 + 单调栈二分)

---
+++ src/main/java/com/oycm/week/lc2026/No519/Solution_4.java
@@ -0,0 +1,24 @@
+package com.oycm.week.lc2026.No519;
+
+public class Solution_4 {
+
+    /**
+     * 统计影子数对 II
+     *
+     * @param nums
+     * @return
+     */
+    public int shadowPairs(int[] nums) {
+        /*
+        给你一个长度为 n 的整数数组 nums。
+        如果一对下标 (i, j) 满足以下所有条件，则称其为一个影子对：
+            0 <= i < j < n
+            nums[i] < nums[j]
+        不存在下标 k，使得 i < k < j 且 nums[i] < nums[k] < nums[j]。
+        返回影子对的总数。
+         */
+        return 0;
+    }
+
+
+}
